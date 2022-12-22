127ef737eb216f651b589caac0d790e53b237e7d
Alan Bertl <alan@unstructured.io>
2022-12-22 17:23:51 -0600

    Remove tooling to dl models from s3

--- scripts/dl-models.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-aws s3 cp s3://utic-dev-models/oer_checkbox/detectron2_oer_checkbox.json "${1:-.models}"/detectron2_oer_checkbox.json
-aws s3 cp s3://utic-dev-models/oer_checkbox/detectron2_finetuned_oer_checkbox.pth  "${1:-.models}"/detectron2_finetuned_oer_checkbox.pth
--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -2,8 +2,6 @@
 
 set -euo pipefail
 
-./scripts/dl-models.sh
-
 DOCKER_BUILDKIT=1 docker buildx build --platform=linux/amd64 -f docker/Dockerfile \
   --progress plain \
   -t ml-inference-dev:latest .
