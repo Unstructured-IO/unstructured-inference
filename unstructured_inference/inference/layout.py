649bc24d903fc49ca24190a6c60b13885d4c577c
Alan Bertl <alan@unstructured.io>
2023-01-12 09:44:42 -0600

    Add processing steps for images

--- unstructured_inference/inference/layout.py
+++ unstructured_inference/inference/layout.py
@@ -159,21 +159,29 @@ class PageLayout:
         return self.image_array
 
 
-def process_data_with_model(data: BinaryIO, model_name: str) -> DocumentLayout:
+def process_data_with_model(
+    data: BinaryIO, model_name: str, is_image: bool = False
+) -> DocumentLayout:
     """Processes pdf file in the form of a file handler (supporting a read method) into a
     DocumentLayout by using a model identified by model_name."""
     with tempfile.NamedTemporaryFile() as tmp_file:
         tmp_file.write(data.read())
-        layout = process_file_with_model(tmp_file.name, model_name)
+        layout = process_file_with_model(tmp_file.name, model_name, is_image=is_image)
 
     return layout
 
 
-def process_file_with_model(filename: str, model_name: str) -> DocumentLayout:
+def process_file_with_model(
+    filename: str, model_name: str, is_image: bool = False
+) -> DocumentLayout:
     """Processes pdf file with name filename into a DocumentLayout by using a model identified by
     model_name."""
     model = None if model_name is None else get_model(model_name)
-    layout = DocumentLayout.from_file(filename, model=model)
+    layout = (
+        DocumentLayout.from_image_file(filename, model=model)
+        if is_image
+        else DocumentLayout.from_file(filename, model=model)
+    )
     return layout
 
 
