efc45fc30b1efe6f062c80e070f64d11a8533288
Alan Bertl <alan@unstructured.io>
2023-01-26 17:33:07 -0600

    Add init docstring explaining model

--- unstructured_inference/models/unstructuredmodel.py
+++ unstructured_inference/models/unstructuredmodel.py
@@ -1,14 +1,17 @@
 from abc import ABC, abstractmethod
 from typing import Any
-from PIL import Image
 
 
 class UnstructuredModel(ABC):
     """Wrapper class for the various models used by unstructured."""
 
     def __init__(self, model: Any):
+        """model should support inference of some sort, either by calling or by some method.
+        UnstructuredModel doesn't provide any training interface, it's assumed the model is
+        already trained.
+        """
         self.model = model
 
     @abstractmethod
-    def __call__(self, x: Image) -> Any:
+    def __call__(self, x: Any) -> Any:
         pass  # pragma: no cover
