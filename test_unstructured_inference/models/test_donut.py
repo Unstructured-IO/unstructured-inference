07468f6a843c2f836a2f22d6c14b55f466c56c16
Sebastian Laverde <sebastian@unstructured.io>
2023-02-03 15:21:08 +0100

    test: add 100% coverage tests for donut prediction

---
+++ test_unstructured_inference/models/test_donut.py
@@ -0,0 +1,69 @@
+import pytest
+import unstructured_inference.models.donut as donut
+
+from transformers import DonutSwinModel
+from PIL import Image
+
+
+@pytest.mark.parametrize(
+    "model_path, processor_path, config_path",
+    [
+        ("crispy_donut_path", "crispy_proc", "crispy_config"),
+        ("cherry_donut_path", "cherry_proc", "cherry_config"),
+    ],
+)
+def test_load_donut_model_raises_when_not_available(model_path, processor_path, config_path):
+    with pytest.raises(ImportError):
+        donut_model = donut.UnstructuredDonutModel()
+        donut_model.initialize(
+            model=model_path, processor=processor_path, config=config_path, task_prompt="<s>"
+        )
+
+
+@pytest.mark.parametrize(
+    "model_path, processor_path, config_path",
+    [
+        (
+            "unstructuredio/donut-base-sroie",
+            "unstructuredio/donut-base-sroie",
+            "unstructuredio/donut-base-sroie",
+        ),
+    ],
+)
+def test_load_donut_model(model_path, processor_path, config_path):
+    donut_model = donut.UnstructuredDonutModel()
+    donut_model.initialize(
+        model=model_path, processor=processor_path, config=config_path, task_prompt="<s>"
+    )
+    assert type(donut_model.model.encoder) == DonutSwinModel
+
+
+@pytest.fixture
+def sample_receipt_transcript():
+    return {
+        "total": "46.00",
+        "date": "20/03/2018",
+        "company": "UROKO JAPANESE CUISINE SDN BHD",
+        "address": "22A-1, JALAN 17/54, SECTION 17, 46400 PETALING JAYA, SELANGOR.",
+    }
+
+
+@pytest.mark.parametrize(
+    "model_path, processor_path, config_path",
+    [
+        (
+            "unstructuredio/donut-base-sroie",
+            "unstructuredio/donut-base-sroie",
+            "unstructuredio/donut-base-sroie",
+        ),
+    ],
+)
+def test_donut_prediction(model_path, processor_path, config_path, sample_receipt_transcript):
+    donut_model = donut.UnstructuredDonutModel()
+    donut_model.initialize(
+        model=model_path, processor=processor_path, config=config_path, task_prompt="<s>"
+    )
+    image_path = "./sample-docs/receipt-sample.jpg"
+    with Image.open(image_path) as image:
+        prediction = donut_model.predict(image)
+        assert prediction == sample_receipt_transcript
