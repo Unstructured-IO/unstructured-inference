8786ad393fc98f0ce1fc84fe4e332e8703dba241
Alan Bertl <alan@unstructured.io>
2023-02-18 12:42:31 -0600

    Remove tests that dont test the api, update yolox specific tests to new interface

--- test_unstructured_inference/test_api.py
+++ test_unstructured_inference/test_api.py
@@ -1,14 +1,11 @@
 import os
-import shutil
 
-import jsons
 import pytest
 from fastapi.testclient import TestClient
 
 from unstructured_inference import api
 from unstructured_inference.models import base as models
 from unstructured_inference.inference.layout import DocumentLayout
-from unstructured_inference.models.yolox import yolox_local_inference  # DocumentLayout #maybe
 
 
 class MockModel:
@@ -57,7 +54,7 @@ def test_bad_route_404():
     assert response.status_code == 404
 
 
-def test_layout_v02_api_parsing_image():
+def test_layout_yolox_api_parsing_image():
     filename = os.path.join("sample-docs", "test-image.jpg")
 
     client = TestClient(api.app)
@@ -66,10 +63,10 @@ def test_layout_v02_api_parsing_image():
         headers={"Accept": "multipart/mixed"},
         files=[("file", (filename, open(filename, "rb"), "image/png"))],
     )
-    doc_layout = jsons.load(response.json(), DocumentLayout)
-    assert len(doc_layout.pages) == 1
+    doc_layout = response.json()
+    assert len(doc_layout["pages"]) == 1
     # NOTE(benjamin) The example sent to the test contains 13 detections
-    assert len(doc_layout.pages[0]["layout"]) == 13
+    assert len(doc_layout["pages"][0]["elements"]) == 13
     assert response.status_code == 200
 
 
@@ -81,14 +78,14 @@ def test_layout_v02_api_parsing_pdf():
         "/layout/yolox/pdf",
         files={"file": (filename, open(filename, "rb"))},
     )
-    doc_layout = jsons.load(response.json(), DocumentLayout)
-    assert len(doc_layout.pages) == 1
+    doc_layout = response.json()
+    assert len(doc_layout["pages"]) == 1
     # NOTE(benjamin) The example sent to the test contains 5 detections
-    assert len(doc_layout.pages[0]["layout"]) == 5
+    assert len(doc_layout["pages"][0]["elements"]) == 5
     assert response.status_code == 200
 
 
-def test_layout_v02_api_parsing_pdf_ocr():
+def test_layout_yolox_api_parsing_pdf_ocr():
     filename = os.path.join("sample-docs", "non-embedded.pdf")
 
     client = TestClient(api.app)
@@ -97,45 +94,10 @@ def test_layout_v02_api_parsing_pdf_ocr():
         files={"file": (filename, open(filename, "rb"))},
         data={"force_ocr": True},
     )
-    doc_layout = jsons.load(response.json(), DocumentLayout)
-    assert len(doc_layout.pages) == 10
-    assert len(doc_layout.pages[0]["layout"]) > 1
+    doc_layout = response.json()
+    assert len(doc_layout["pages"]) == 10
+    assert len(doc_layout["pages"][0]["elements"]) > 1
     assert response.status_code == 200
-
-
-def test_layout_v02_local_parsing_image():
-    filename = os.path.join("sample-docs", "test-image.jpg")
-    OUTPUT_DIR = "yolox_output"
-    # NOTE(benjamin) keep_output = True create a file for each image in
-    # localstorage for visualization of the result
-    if os.path.exists(OUTPUT_DIR):
-        # NOTE(benjamin): should delete the default output folder on test?
-        shutil.rmtree(OUTPUT_DIR)
-    document_layout_1 = yolox_local_inference(filename, type="image", output_directory=OUTPUT_DIR)
-    assert len(document_layout_1.pages) == 1
-    document_layout_2 = yolox_local_inference(filename, type="image")
-    # NOTE(benjamin) The example image should result in one page result
-    assert len(document_layout_2.pages) == 1
-    # NOTE(benjamin) The example sent to the test contains 13 detections
-    assert len(document_layout_2.pages[0].layout) == 13
-
-
-def test_layout_v02_local_parsing_pdf():
-    filename = os.path.join("sample-docs", "loremipsum.pdf")
-    document_layout = yolox_local_inference(filename, type="pdf")
-    content = document_layout.to_string()
-    assert "Lorem ipsum" in content
-    assert len(document_layout.pages) == 1
-    # NOTE(benjamin) The example sent to the test contains 5 detections
-    assert len(document_layout.pages[0].layout) == 5
-
-
-def test_layout_v02_local_parsing_empty_pdf():
-    filename = os.path.join("sample-docs", "empty-document.pdf")
-    document_layout = yolox_local_inference(filename, type="pdf")
-    assert len(document_layout.pages) == 1
-    # NOTE(benjamin) The example sent to the test contains 5 detections
-    assert len(document_layout.pages[0].layout) == 0
 
 
 def test_healthcheck(monkeypatch):
