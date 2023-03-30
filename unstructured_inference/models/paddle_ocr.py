6b2d6104a128136f8ea72769254acb5e2607cda9
Alan Bertl <alan@unstructured.io>
2023-03-30 11:45:23 -0500

    Stop importing paddleocr unless needed

--- unstructured_inference/models/paddle_ocr.py
+++ unstructured_inference/models/paddle_ocr.py
@@ -1,10 +1,10 @@
-from unstructured_paddleocr import PaddleOCR
-
-paddle_ocr: PaddleOCR = None
+paddle_ocr = None  # type: ignore
 
 
 def load_agent():
     """Loads the PaddleOCR agent as a global variable to ensure that we only load it once."""
+
+    from unstructured_paddleocr import PaddleOCR
 
     global paddle_ocr
     paddle_ocr = PaddleOCR(use_angle_cls=True, lang="en", mkl_dnn=True, show_log=False)
