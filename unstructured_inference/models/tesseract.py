a4d64cf7bb0a8e693d363431ad083146a8a2fb6f
Matt Robinson <mrobinson@unstructuredai.io>
2023-04-20 10:40:49 -0400

    add language parameter to tesseract

--- unstructured_inference/models/tesseract.py
+++ unstructured_inference/models/tesseract.py
@@ -5,8 +5,15 @@ from unstructured_inference.logger import logger
 ocr_agent: TesseractAgent = None
 
 
-def load_agent():
-    """Loads the Tesseract OCR agent as a global variable to ensure that we only load it once."""
+def load_agent(languages: str = "eng"):
+    """Loads the Tesseract OCR agent as a global variable to ensure that we only load it once.
+
+    Parameters
+    ----------
+    languages
+        The languages to use for the Tesseract agent. To use a langauge, you'll first need
+        to isntall the appropriate Tesseract language pack.
+    """
     global ocr_agent
 
     if not is_pytesseract_available():
@@ -17,4 +24,4 @@ def load_agent():
 
     if ocr_agent is None:
         logger.info("Loading the Tesseract OCR agent ...")
-        ocr_agent = TesseractAgent(languages="eng")
+        ocr_agent = TesseractAgent(languages=languages)
