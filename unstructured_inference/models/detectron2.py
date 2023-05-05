f48e06f73a4b7734202cea6eb41d98c8d4820262
Benjamin Torres <benjamin@unstructured.io>
2023-05-05 17:29:26 -0600

    Added ONNX model for detectron2

--- unstructured_inference/models/detectron2.py
+++ unstructured_inference/models/detectron2.py
@@ -13,6 +13,9 @@ from unstructured_inference.logger import logger
 from unstructured_inference.inference.layoutelement import LayoutElement
 from unstructured_inference.models.unstructuredmodel import UnstructuredModel
 from unstructured_inference.utils import LazyDict, LazyEvaluateInfo
+import onnxruntime
+import numpy as np
+import cv2
 
 
 DETECTRON_CONFIG: Final = "lp://PubLayNet/faster_rcnn_R_50_FPN_3x/config"
@@ -43,6 +46,15 @@ MODEL_TYPES = {
         label_map=DEFAULT_LABEL_MAP,
         extra_config=DEFAULT_EXTRA_CONFIG,
     ),
+    "onnx":LazyDict(
+        model_path=LazyEvaluateInfo(
+            hf_hub_download,
+            "unstructuredio/detectron2_faster_rcnn_R_50_FPN_3x",
+            "model.onnx",
+        ),
+        label_map=DEFAULT_LABEL_MAP,
+        extra_config=DEFAULT_EXTRA_CONFIG,
+    ),
     "checkbox": LazyDict(
         model_path=LazyEvaluateInfo(
             hf_hub_download, "unstructuredio/oer-checkbox", "detectron2_finetuned_oer_checkbox.pth"
@@ -62,7 +74,7 @@ class UnstructuredDetectronModel(UnstructuredModel):
     def predict(self, x: Image):
         """Makes a prediction using detectron2 model."""
         super().predict(x)
-        prediction = self.model.detect(x)
+        prediction= self.image_processing(x)#[LayoutElement.from_lp_textblock(block) for block in prediction]
         return [LayoutElement.from_lp_textblock(block) for block in prediction]
 
     def initialize(
@@ -84,10 +96,60 @@ class UnstructuredDetectronModel(UnstructuredModel):
         config_path_str = str(config_path)
         model_path_str: Optional[str] = None if model_path is None else str(model_path)
         logger.info("Loading the Detectron2 layout model ...")
-        self.model = Detectron2LayoutModel(
-            config_path_str,
-            model_path=model_path_str,
-            label_map=label_map,
-            extra_config=extra_config,
-            device=device,
-        )
+        self.model = onnxruntime.InferenceSession(model_path, providers=["CPUExecutionProvider"])
+
+    def image_processing(
+        self,
+        image: Image = None,
+    ) -> List[LayoutElement]:
+        """Method runing YoloX for layout detection, returns a PageLayout
+        parameters
+        ----------
+        page
+            Path for image file with the image to process
+        origin_img
+            If specified, an Image object for process with YoloX model
+        page_number
+            Number asigned to the PageLayout returned
+        output_directory
+            Boolean indicating if result will be stored
+        """
+        # The model was trained and exported with this shape
+        # TODO (benjamin): check other shapes for inference
+        input_shape = (1035, 800) # detectron2 specific
+        img = np.array(image)
+        #img, ratio = preprocess(origin_img, input_shape)
+        # TODO (benjamin): We should use models.get_model() but currenly returns Detectron model
+        session = self.model
+        #onnx input expected
+        # [3,1035,800]
+        img = cv2.resize(
+                        img,
+                        (800, 1035),
+                        interpolation=cv2.INTER_LINEAR,
+                        ).astype(np.float32)
+        img=img.transpose(2,0,1)
+        ort_inputs = {session.get_inputs()[0].name: img}
+        output = session.run(None, ort_inputs)
+        # output[0] seems like bounding boxes (in pixels, not sure if original size or in (1035,800))
+        # output boxes seems like xywh due similar 3rd entry in all elements (same width)
+        # output[1] seems like labels for bboxes
+        # output[2] seems like confidence score for each label
+        # output[3] seems like image size (it's fixed to (1035,800) so this info is useless now)
+        bboxes = output[0]
+        labels = output[1]
+        confidence = output[2]
+        
+        regions = []
+        for bbox,label,conf in zip(bboxes,labels,confidence):
+            # Each detection should have (x1,y1,x2,y2,probability,class) format
+            # being (x1,y1) the top left and (x2,y2) the bottom right
+            detected_class = DEFAULT_LABEL_MAP[int(label)]
+            region = LayoutElement(bbox[0], bbox[1], bbox[0]+bbox[2], bbox[1]+bbox[3], text=None, type=detected_class)
+
+            regions.append(region)
+
+        regions.sort(key=lambda element: element.y1)
+
+        return regions
+        
