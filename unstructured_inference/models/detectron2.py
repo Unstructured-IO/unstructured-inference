b3fa4c36e215971b78ddf9624cfe6611e196f0b6
Alan Bertl <alan@unstructured.io>
2023-05-07 01:29:50 -0500

    Add confidence threshold, remove checkbox

--- unstructured_inference/models/detectron2.py
+++ unstructured_inference/models/detectron2.py
@@ -1,4 +1,4 @@
-from typing import Final, Optional, Union, List, Dict, Any
+from typing import Final, Optional, Union, List, Dict
 from pathlib import Path
 
 from PIL import Image
@@ -13,7 +13,6 @@ import numpy as np
 import cv2
 
 
-DETECTRON_CONFIG: Final = "lp://PubLayNet/faster_rcnn_R_50_FPN_3x/config"
 DEFAULT_LABEL_MAP: Final[Dict[int, str]] = {
     0: "Text",
     1: "Title",
@@ -21,12 +20,11 @@ DEFAULT_LABEL_MAP: Final[Dict[int, str]] = {
     3: "Table",
     4: "Figure",
 }
-DEFAULT_EXTRA_CONFIG: Final[List[Any]] = ["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8]
 
 
 # NOTE(alan): Entries are implemented as LazyDicts so that models aren't downloaded until they are
 # needed.
-MODEL_TYPES = {
+MODEL_TYPES: Dict[Optional[str], LazyDict] = {
     None: LazyDict(
         model_path=LazyEvaluateInfo(
             hf_hub_download,
@@ -34,17 +32,7 @@ MODEL_TYPES = {
             "model.onnx",
         ),
         label_map=DEFAULT_LABEL_MAP,
-        # extra_config=DEFAULT_EXTRA_CONFIG,
-    ),
-    "checkbox": LazyDict(
-        model_path=LazyEvaluateInfo(
-            hf_hub_download, "unstructuredio/oer-checkbox", "detectron2_finetuned_oer_checkbox.pth"
-        ),
-        config_path=LazyEvaluateInfo(
-            hf_hub_download, "unstructuredio/oer-checkbox", "detectron2_oer_checkbox.json"
-        ),
-        label_map={0: "Unchecked", 1: "Checked"},
-        extra_config=None,
+        confidence_threshold=0.8,
     ),
 }
 
@@ -52,7 +40,7 @@ MODEL_TYPES = {
 class UnstructuredDetectronModel(UnstructuredModel):
     """Unstructured model wrapper for Detectron2LayoutModel."""
 
-    def predict(self, x: Image):
+    def predict(self, x: Image.Image):
         """Makes a prediction using detectron2 model."""
         super().predict(x)
         prediction = self.image_processing(x)
@@ -60,17 +48,21 @@ class UnstructuredDetectronModel(UnstructuredModel):
 
     def initialize(
         self,
-        model_path: Optional[Union[str, Path]] = None,
-        label_map: Optional[Dict[int, str]] = None,
+        model_path: Union[str, Path],
+        label_map: Dict[int, str],
+        confidence_threshold: Optional[float] = None,
     ):
         """Loads the detectron2 model using the specified parameters"""
         logger.info("Loading the Detectron2 layout model ...")
         self.model = onnxruntime.InferenceSession(model_path, providers=["CPUExecutionProvider"])
         self.label_map = label_map
+        if confidence_threshold is None:
+            confidence_threshold = 0.5
+        self.confidence_threshold = confidence_threshold
 
     def image_processing(
         self,
-        image: Image = None,
+        image: Image.Image,
     ) -> List[LayoutElement]:
         """Method runing YoloX for layout detection, returns a PageLayout
         parameters
@@ -106,19 +98,13 @@ class UnstructuredDetectronModel(UnstructuredModel):
         # output[1] seems like labels for bboxes
         # output[2] seems like confidence score for each label
         # output[3] seems like image size (it's fixed to (1035,800) so this info is useless now)
-        bboxes, labels, confidence, _ = output
-        bboxes = bboxes.tolist()
-        labels = labels.tolist()
-        confidence = confidence.tolist()
+        bboxes, labels, confidence_scores, _ = output
 
         regions = []
-        for (x1, y1, w, h), label, conf in zip(bboxes, labels, confidence):
-            # Each detection should have (x1,y1,x2,y2,probability,class) format
-            # being (x1,y1) the top left and (x2,y2) the bottom right
+        for (x1, y1, x2, y2), label, conf in zip(bboxes, labels, confidence_scores):
             detected_class = self.label_map[int(label)]
-            print(detected_class, conf)
-            if conf >= 0.8:
-                region = LayoutElement(x1, y1, x1 + w, y1 + h, text=None, type=detected_class)
+            if conf >= self.confidence_threshold:
+                region = LayoutElement(x1, y1, x2, y2, text=None, type=detected_class)
 
                 regions.append(region)
 
