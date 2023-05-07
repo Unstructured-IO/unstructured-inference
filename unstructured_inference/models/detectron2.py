92a064ea9b943dfaee21e29bc4c62f8813a0494f
Alan Bertl <alan@unstructured.io>
2023-05-06 23:26:43 -0500

    make onnx model default

--- unstructured_inference/models/detectron2.py
+++ unstructured_inference/models/detectron2.py
@@ -1,8 +1,6 @@
 from typing import Final, Optional, Union, List, Dict, Any
 from pathlib import Path
 
-from layoutparser.models.detectron2.layoutmodel import is_detectron2_available
-from layoutparser.models.model_config import LayoutModelConfig
 from PIL import Image
 from huggingface_hub import hf_hub_download
 
@@ -32,30 +30,11 @@ MODEL_TYPES = {
     None: LazyDict(
         model_path=LazyEvaluateInfo(
             hf_hub_download,
-            "layoutparser/detectron2",
-            "PubLayNet/faster_rcnn_R_50_FPN_3x/model_final.pth",
-        ),
-        config_path=LazyEvaluateInfo(
-            hf_hub_download,
-            "layoutparser/detectron2",
-            "PubLayNet/faster_rcnn_R_50_FPN_3x/config.yml",
-        ),
-        label_map=DEFAULT_LABEL_MAP,
-        extra_config=DEFAULT_EXTRA_CONFIG,
-    ),
-    "onnx": LazyDict(
-        model_path=LazyEvaluateInfo(
-            hf_hub_download,
             "unstructuredio/detectron2_faster_rcnn_R_50_FPN_3x",
             "model.onnx",
         ),
-        config_path=LazyEvaluateInfo(
-            hf_hub_download,
-            "layoutparser/detectron2",
-            "PubLayNet/faster_rcnn_R_50_FPN_3x/config.yml",
-        ),
         label_map=DEFAULT_LABEL_MAP,
-        extra_config=DEFAULT_EXTRA_CONFIG,
+        # extra_config=DEFAULT_EXTRA_CONFIG,
     ),
     "checkbox": LazyDict(
         model_path=LazyEvaluateInfo(
@@ -76,29 +55,18 @@ class UnstructuredDetectronModel(UnstructuredModel):
     def predict(self, x: Image):
         """Makes a prediction using detectron2 model."""
         super().predict(x)
-        prediction = self.image_processing(
-            x
-        )  # [LayoutElement.from_lp_textblock(block) for block in prediction]
-        return prediction  # [LayoutElement.from_lp_textblock(block) for block in prediction]
+        prediction = self.image_processing(x)
+        return prediction
 
     def initialize(
         self,
-        config_path: Union[str, Path, LayoutModelConfig],
         model_path: Optional[Union[str, Path]] = None,
         label_map: Optional[Dict[int, str]] = None,
-        extra_config: Optional[list] = None,
-        device: Optional[str] = None,
     ):
         """Loads the detectron2 model using the specified parameters"""
-
-        if not is_detectron2_available():
-            raise ImportError(
-                "Failed to load the Detectron2 model. Ensure that the Detectron2 "
-                "module is correctly installed."
-            )
-
         logger.info("Loading the Detectron2 layout model ...")
         self.model = onnxruntime.InferenceSession(model_path, providers=["CPUExecutionProvider"])
+        self.label_map = label_map
 
     def image_processing(
         self,
@@ -118,16 +86,15 @@ class UnstructuredDetectronModel(UnstructuredModel):
         """
         # The model was trained and exported with this shape
         # TODO (benjamin): check other shapes for inference
-        # input_shape = (1035, 800)  # detectron2 specific
+        input_shape = (800, 1035)  # detectron2 specific
         img = np.array(image)
-        # img, ratio = preprocess(origin_img, input_shape)
         # TODO (benjamin): We should use models.get_model() but currenly returns Detectron model
         session = self.model
         # onnx input expected
         # [3,1035,800]
         img = cv2.resize(
             img,
-            (800, 1035),
+            input_shape,
             interpolation=cv2.INTER_LINEAR,
         ).astype(np.float32)
         img = img.transpose(2, 0, 1)
@@ -139,25 +106,21 @@ class UnstructuredDetectronModel(UnstructuredModel):
         # output[1] seems like labels for bboxes
         # output[2] seems like confidence score for each label
         # output[3] seems like image size (it's fixed to (1035,800) so this info is useless now)
-        bboxes = output[0].tolist()
-        labels = output[1].tolist()
-        confidence = output[2].tolist()
+        bboxes, labels, confidence, _ = output
+        bboxes = bboxes.tolist()
+        labels = labels.tolist()
+        confidence = confidence.tolist()
 
         regions = []
-        for bbox, label, conf in zip(bboxes, labels, confidence):
+        for (x1, y1, w, h), label, conf in zip(bboxes, labels, confidence):
             # Each detection should have (x1,y1,x2,y2,probability,class) format
             # being (x1,y1) the top left and (x2,y2) the bottom right
-            detected_class = DEFAULT_LABEL_MAP[int(label)]
-            region = LayoutElement(
-                bbox[0],
-                bbox[1],
-                bbox[0] + bbox[2],
-                bbox[1] + bbox[3],
-                text=None,
-                type=detected_class,
-            )
+            detected_class = self.label_map[int(label)]
+            print(detected_class, conf)
+            if conf >= 0.8:
+                region = LayoutElement(x1, y1, x1 + w, y1 + h, text=None, type=detected_class)
 
-            regions.append(region)
+                regions.append(region)
 
         regions.sort(key=lambda element: element.y1)
 
