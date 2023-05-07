51c4d46adcaab3b4def0de62f3e12a81cc50624f
Alan Bertl <alan@unstructured.io>
2023-05-06 23:26:58 -0500

    update tests

--- test_unstructured_inference/models/test_detectron2.py
+++ test_unstructured_inference/models/test_detectron2.py
@@ -10,38 +10,31 @@ class MockDetectron2LayoutModel:
         self.args = args
         self.kwargs = kwargs
 
-    def detect(self, x):
+    def image_processing(self, x):
         return []
 
 
-def test_load_default_model(monkeypatch):
-    monkeypatch.setattr(detectron2, "Detectron2LayoutModel", MockDetectron2LayoutModel)
-
-    with patch.object(detectron2, "is_detectron2_available", return_value=True):
+def test_load_default_model():
+    with patch.object(detectron2.onnxruntime, "InferenceSession", new=MockDetectron2LayoutModel):
         model = models.get_model()
 
     assert isinstance(model.model, MockDetectron2LayoutModel)
 
 
-def test_load_default_model_raises_when_not_available():
-    with patch.object(detectron2, "is_detectron2_available", return_value=False):
-        with pytest.raises(ImportError):
-            models.get_model()
-
-
-@pytest.mark.parametrize("config_path, model_path", [("asdf", "diufs"), ("dfaw", "hfhfhfh")])
-def test_load_model(monkeypatch, config_path, model_path):
-    monkeypatch.setattr(detectron2, "Detectron2LayoutModel", MockDetectron2LayoutModel)
-    with patch.object(detectron2, "is_detectron2_available", return_value=True):
+@pytest.mark.parametrize(("model_path", "label_map"), [("asdf", "diufs"), ("dfaw", "hfhfhfh")])
+def test_load_model(model_path, label_map):
+    with patch.object(detectron2.onnxruntime, "InferenceSession", return_value=True):
         model = detectron2.UnstructuredDetectronModel()
-        model.initialize(config_path=config_path, model_path=model_path)
-    assert config_path == model.model.args[0]
-    assert model_path == model.model.kwargs["model_path"]
+        model.initialize(model_path=model_path, label_map=label_map)
+        args, _ = detectron2.onnxruntime.InferenceSession.call_args
+        assert args == (model_path,)
+    assert label_map == model.label_map
 
 
 def test_unstructured_detectron_model():
     model = detectron2.UnstructuredDetectronModel()
     model.model = MockDetectron2LayoutModel()
-    result = model(None)
+    with patch.object(detectron2.UnstructuredDetectronModel, "image_processing", return_value=[]):
+        result = model(None)
     assert isinstance(result, list)
     assert len(result) == 0
