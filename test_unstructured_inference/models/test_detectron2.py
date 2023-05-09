615e526d0945038d13c524d1eee467047d3d299b
Alan Bertl <alan@unstructured.io>
2023-05-09 00:34:36 -0500

    Make original detectron2 and onnx separate models

--- test_unstructured_inference/models/test_detectron2.py
+++ test_unstructured_inference/models/test_detectron2.py
@@ -1,8 +1,5 @@
-import os
-from unittest.mock import patch
-
 import pytest
-from PIL import Image
+from unittest.mock import patch
 
 import unstructured_inference.models.detectron2 as detectron2
 import unstructured_inference.models.base as models
@@ -13,57 +10,38 @@ class MockDetectron2LayoutModel:
         self.args = args
         self.kwargs = kwargs
 
-    def run(self, *args):
-        return ([(1, 2, 3, 4)], [0], [0.818], [(4, 5)])
-
-    def get_inputs(self):
-        class input_thing:
-            name = "Bernard"
-
-        return [input_thing()]
+    def detect(self, x):
+        return []
 
 
-def test_load_default_model():
-    with patch.object(detectron2.onnxruntime, "InferenceSession", new=MockDetectron2LayoutModel):
+def test_load_default_model(monkeypatch):
+    monkeypatch.setattr(detectron2, "Detectron2LayoutModel", MockDetectron2LayoutModel)
+
+    with patch.object(detectron2, "is_detectron2_available", return_value=True):
         model = models.get_model()
 
     assert isinstance(model.model, MockDetectron2LayoutModel)
 
 
-@pytest.mark.parametrize(("model_path", "label_map"), [("asdf", "diufs"), ("dfaw", "hfhfhfh")])
-def test_load_model(model_path, label_map):
-    with patch.object(detectron2.onnxruntime, "InferenceSession", return_value=True):
+def test_load_default_model_raises_when_not_available():
+    with patch.object(detectron2, "is_detectron2_available", return_value=False):
+        with pytest.raises(ImportError):
+            models.get_model()
+
+
+@pytest.mark.parametrize("config_path, model_path", [("asdf", "diufs"), ("dfaw", "hfhfhfh")])
+def test_load_model(monkeypatch, config_path, model_path):
+    monkeypatch.setattr(detectron2, "Detectron2LayoutModel", MockDetectron2LayoutModel)
+    with patch.object(detectron2, "is_detectron2_available", return_value=True):
         model = detectron2.UnstructuredDetectronModel()
-        model.initialize(model_path=model_path, label_map=label_map)
-        args, _ = detectron2.onnxruntime.InferenceSession.call_args
-        assert args == (model_path,)
-    assert label_map == model.label_map
+        model.initialize(config_path=config_path, model_path=model_path)
+    assert config_path == model.model.args[0]
+    assert model_path == model.model.kwargs["model_path"]
 
 
 def test_unstructured_detectron_model():
     model = detectron2.UnstructuredDetectronModel()
-    model.model = 1
-    with patch.object(detectron2.UnstructuredDetectronModel, "predict", return_value=[]):
-        result = model(None)
+    model.model = MockDetectron2LayoutModel()
+    result = model(None)
     assert isinstance(result, list)
     assert len(result) == 0
-
-
-def test_inference():
-    with patch.object(
-        detectron2.onnxruntime, "InferenceSession", return_value=MockDetectron2LayoutModel()
-    ):
-        model = detectron2.UnstructuredDetectronModel()
-        model.initialize(model_path="test_path", label_map={0: "test_class"})
-        assert isinstance(model.model, MockDetectron2LayoutModel)
-        with open(os.path.join("sample-docs", "receipt-sample.jpg"), mode="rb") as fp:
-            image = Image.open(fp)
-            image.load()
-        elements = model(image)
-        assert len(elements) == 1
-        element = elements[0]
-        (x1, y1), _, (x2, y2), _ = element.coordinates
-        # NOTE(alan): The bbox coordinates get resized, so check their relative proportions
-        assert x2 / x1 == pytest.approx(3.0)  # x1 == 1, x2 == 3 before scaling
-        assert y2 / y1 == pytest.approx(2.0)  # y1 == 2, y2 == 4 before scaling
-        assert element.type == "test_class"
--- unstructured_inference/models/detectron2.py
+++ unstructured_inference/models/detectron2.py
@@ -1,6 +1,11 @@
-from typing import Final, Optional, Union, Dict, List
+from typing import Final, Optional, Union, List, Dict, Any
 from pathlib import Path
 
+from layoutparser.models.detectron2.layoutmodel import (
+    is_detectron2_available,
+    Detectron2LayoutModel,
+)
+from layoutparser.models.model_config import LayoutModelConfig
 from PIL import Image
 from huggingface_hub import hf_hub_download
 
@@ -8,11 +13,9 @@ from unstructured_inference.logger import logger
 from unstructured_inference.inference.layoutelement import LayoutElement
 from unstructured_inference.models.unstructuredmodel import UnstructuredModel
 from unstructured_inference.utils import LazyDict, LazyEvaluateInfo
-import onnxruntime
-import numpy as np
-import cv2
 
 
+DETECTRON_CONFIG: Final = "lp://PubLayNet/faster_rcnn_R_50_FPN_3x/config"
 DEFAULT_LABEL_MAP: Final[Dict[int, str]] = {
     0: "Text",
     1: "Title",
@@ -20,19 +23,35 @@ DEFAULT_LABEL_MAP: Final[Dict[int, str]] = {
     3: "Table",
     4: "Figure",
 }
+DEFAULT_EXTRA_CONFIG: Final[List[Any]] = ["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8]
 
 
 # NOTE(alan): Entries are implemented as LazyDicts so that models aren't downloaded until they are
 # needed.
-MODEL_TYPES: Dict[Optional[str], LazyDict] = {
+MODEL_TYPES = {
     None: LazyDict(
         model_path=LazyEvaluateInfo(
             hf_hub_download,
-            "unstructuredio/detectron2_faster_rcnn_R_50_FPN_3x",
-            "model.onnx",
+            "layoutparser/detectron2",
+            "PubLayNet/faster_rcnn_R_50_FPN_3x/model_final.pth",
+        ),
+        config_path=LazyEvaluateInfo(
+            hf_hub_download,
+            "layoutparser/detectron2",
+            "PubLayNet/faster_rcnn_R_50_FPN_3x/config.yml",
         ),
         label_map=DEFAULT_LABEL_MAP,
-        confidence_threshold=0.8,
+        extra_config=DEFAULT_EXTRA_CONFIG,
+    ),
+    "checkbox": LazyDict(
+        model_path=LazyEvaluateInfo(
+            hf_hub_download, "unstructuredio/oer-checkbox", "detectron2_finetuned_oer_checkbox.pth"
+        ),
+        config_path=LazyEvaluateInfo(
+            hf_hub_download, "unstructuredio/oer-checkbox", "detectron2_oer_checkbox.json"
+        ),
+        label_map={0: "Unchecked", 1: "Checked"},
+        extra_config=None,
     ),
 }
 
@@ -40,79 +59,35 @@ MODEL_TYPES: Dict[Optional[str], LazyDict] = {
 class UnstructuredDetectronModel(UnstructuredModel):
     """Unstructured model wrapper for Detectron2LayoutModel."""
 
-    # The model was trained and exported with this shape
-    required_w = 800
-    required_h = 1035
-
-    def predict(self, image: Image.Image) -> List[LayoutElement]:
+    def predict(self, x: Image):
         """Makes a prediction using detectron2 model."""
-        super().predict(image)
-
-        prepared_input = self.preprocess(image)
-        bboxes, labels, confidence_scores, _ = self.model.run(None, prepared_input)
-        input_w, input_h = image.size
-        regions = self.postprocess(bboxes, labels, confidence_scores, input_w, input_h)
-
-        return regions
+        super().predict(x)
+        prediction = self.model.detect(x)
+        return [LayoutElement.from_lp_textblock(block) for block in prediction]
 
     def initialize(
         self,
-        model_path: Union[str, Path],
-        label_map: Dict[int, str],
-        confidence_threshold: Optional[float] = None,
+        config_path: Union[str, Path, LayoutModelConfig],
+        model_path: Optional[Union[str, Path]] = None,
+        label_map: Optional[Dict[int, str]] = None,
+        extra_config: Optional[list] = None,
+        device: Optional[str] = None,
     ):
         """Loads the detectron2 model using the specified parameters"""
+
+        if not is_detectron2_available():
+            raise ImportError(
+                "Failed to load the Detectron2 model. Ensure that the Detectron2 "
+                "module is correctly installed."
+            )
+
+        config_path_str = str(config_path)
+        model_path_str: Optional[str] = None if model_path is None else str(model_path)
         logger.info("Loading the Detectron2 layout model ...")
-        self.model = onnxruntime.InferenceSession(model_path, providers=["CPUExecutionProvider"])
-        self.label_map = label_map
-        if confidence_threshold is None:
-            confidence_threshold = 0.5
-        self.confidence_threshold = confidence_threshold
-
-    def preprocess(self, image: Image.Image) -> Dict[str, np.ndarray]:
-        """Process input image into required format for ingestion into the Detectron2 ONNX binary.
-        This involves resizing to a fixed shape and converting to a specific numpy format."""
-        # TODO (benjamin): check other shapes for inference
-        img = np.array(image)
-        # TODO (benjamin): We should use models.get_model() but currenly returns Detectron model
-        session = self.model
-        # onnx input expected
-        # [3,1035,800]
-        img = cv2.resize(
-            img,
-            (self.required_w, self.required_h),
-            interpolation=cv2.INTER_LINEAR,
-        ).astype(np.float32)
-        img = img.transpose(2, 0, 1)
-        ort_inputs = {session.get_inputs()[0].name: img}
-        return ort_inputs
-
-    def postprocess(
-        self,
-        bboxes: np.ndarray,
-        labels: np.ndarray,
-        confidence_scores: np.ndarray,
-        input_w: float,
-        input_h: float,
-    ) -> List[LayoutElement]:
-        """Process output into Unstructured class. Bounding box coordinates are converted to
-        original image resolution."""
-        regions = []
-        width_conversion = input_w / self.required_w
-        height_conversion = input_h / self.required_h
-        for (x1, y1, x2, y2), label, conf in zip(bboxes, labels, confidence_scores):
-            detected_class = self.label_map[int(label)]
-            if conf >= self.confidence_threshold:
-                region = LayoutElement(
-                    x1 * width_conversion,
-                    y1 * height_conversion,
-                    x2 * width_conversion,
-                    y2 * height_conversion,
-                    text=None,
-                    type=detected_class,
-                )
-
-                regions.append(region)
-
-        regions.sort(key=lambda element: element.y1)
-        return regions
+        self.model = Detectron2LayoutModel(
+            config_path_str,
+            model_path=model_path_str,
+            label_map=label_map,
+            extra_config=extra_config,
+            device=device,
+        )
