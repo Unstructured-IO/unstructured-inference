64870dee4bd532ad02a5cf36986f6dab463e2f85
Roman Isecke <136338424+rbiseck3@users.noreply.github.com>
2023-07-25 14:20:15 -0400

    Update os dir naming for images (#157)
    * Update os dir naming for images
    
    * update changelog
    
    * Add Docker approach to testing
    
    * Add unit test to check no ext case

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 PACKAGE_NAME := unstructured_inference
 PIP_VERSION := 23.1.2
+CURRENT_DIR := $(shell pwd)
 
 
 .PHONY: help
@@ -116,3 +117,23 @@ version-sync:
 .PHONY: check-coverage
 check-coverage:
 	coverage report --fail-under=95
+
+##########
+# Docker #
+##########
+
+# Docker targets are provided for convenience only and are not required in a standard development environment
+
+DOCKER_IMAGE ?= unstructured-inference:dev
+
+.PHONY: docker-build
+docker-build:
+	PIP_VERSION=${PIP_VERSION} DOCKER_IMAGE_NAME=${DOCKER_IMAGE} ./scripts/docker-build.sh
+
+.PHONY: docker-test
+docker-test: docker-build
+	docker run --rm \
+	-v ${CURRENT_DIR}/test_unstructured_inference:/home/test_unstructured_inference \
+	-v ${CURRENT_DIR}/sample-docs:/home/sample-docs \
+	$(DOCKER_IMAGE) \
+	bash -c "pytest $(if $(TEST_NAME),-k $(TEST_NAME),) test_unstructured_inference"
--- test_unstructured_inference/inference/test_layout.py
+++ test_unstructured_inference/inference/test_layout.py
@@ -836,7 +836,17 @@ def test_create_image_output_dir():
     with tempfile.TemporaryDirectory() as tmpdir:
         tmp_f_path = os.path.join(tmpdir, "loremipsum.pdf")
         output_dir = create_image_output_dir(tmp_f_path)
-        expected_output_dir = os.path.join(os.path.abspath(tmpdir), "loremipsum")
+        expected_output_dir = os.path.join(os.path.abspath(tmpdir), "loremipsum_images")
+        assert os.path.isdir(output_dir)
+        assert os.path.isabs(output_dir)
+        assert output_dir == expected_output_dir
+
+
+def test_create_image_output_dir_no_ext():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmp_f_path = os.path.join(tmpdir, "loremipsum_no_ext")
+        output_dir = create_image_output_dir(tmp_f_path)
+        expected_output_dir = os.path.join(os.path.abspath(tmpdir), "loremipsum_no_ext_images")
         assert os.path.isdir(output_dir)
         assert os.path.isabs(output_dir)
         assert output_dir == expected_output_dir
