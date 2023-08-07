184e103cc5afe3435a429f86fed5c6a926e3cbcf
christinestraub <christinemstraub@gmail.com>
2023-08-07 11:23:33 -0700

    feat: add functionality to validate ocr performance by `pdf` mode to `validate_ocr_performance` module

--- examples/ocr/validate_ocr_performance.py
+++ examples/ocr/validate_ocr_performance.py
@@ -1,10 +1,14 @@
 import json
 import os
 import tempfile
+import time
 from datetime import datetime
+from difflib import SequenceMatcher
 
 import pdf2image
+import nltk
 from engine import run_ocr_with_layout_detection
+from unstructured_inference.inference.layout import process_file_with_model
 
 from unstructured_inference.models.base import get_model
 from unstructured_inference.models.unstructuredmodel import (
@@ -12,8 +16,11 @@ from unstructured_inference.models.unstructuredmodel import (
     UnstructuredObjectDetectionModel,
 )
 
+# Download the required resources (run this once)
+nltk.download('punkt')
 
-def run(filename, page_limit=None, drawable=True):
+
+def run_analysis(filename, page_limit=None, drawable=True):
     print(">>> Start", filename)
 
     now_dt = datetime.utcnow()
@@ -102,19 +109,81 @@ def run(filename, page_limit=None, drawable=True):
     text_individual = delimiter.join(text_individual_dict.values())
     text_entire = delimiter.join(text_entire_dict.values())
 
+
+def validate_performance(
+    f_name,
+):
+    print(">>> Start performance comparison - filename:", f_name)
+
+    now_dt = datetime.utcnow()
+    now_str = now_dt.strftime("%Y_%m_%d-%H_%M_%S")
+
+    f_path = os.path.join(example_docs_dir, f_name)
+    pdf_info = pdf2image.pdfinfo_from_path(f_path)
+
+    processing_result = {}
+    for ocr_mode in ["individual_blocks", "entire_page"]:
+        start_time = time.time()
+        layout = process_file_with_model(
+            f_path,
+            model_name=None,
+            ocr_mode=ocr_mode,
+        )
+        infer_time = time.time() - start_time
+
+        full_text = str(layout)
+        page_text = {}
+        for page in layout.pages:
+            page_text[page.number] = str(page)
+
+        processing_result[ocr_mode] = {
+            "infer_time": infer_time,
+            "full_text": full_text,
+            "page_text": page_text,
+        }
+
+    individual_mode_page_text = processing_result["individual_blocks"]["page_text"]
+    entire_mode_page_text = processing_result["individual_blocks"]["page_text"]
+    individual_mode_full_text = processing_result["individual_blocks"]["full_text"]
+    entire_mode_full_text = processing_result["entire_page"]["full_text"]
+
+    compare_result = compare_processed_text(individual_mode_full_text, entire_mode_full_text)
+
+    report = {
+        "file_info": {
+            "filename": f_name,
+            "n_pages": pdf_info["Pages"],
+        },
+        "processing_time": {
+            "individual_blocks": processing_result["individual_blocks"]["infer_time"],
+            "entire_page": processing_result["entire_page"]["infer_time"],
+        },
+        "text_similarity": compare_result,
+        "extracted_text": {
+            "individual_blocks": {
+                "page_text": individual_mode_page_text,
+                "full_text": individual_mode_full_text,
+            },
+            "entire_page": {
+                "page_text": entire_mode_page_text,
+                "full_text": entire_mode_full_text,
+            },
+        },
+    }
+
+    write_report(report, now_str)
+
+    print("<<< End performance comparison", f_name)
+
+
+def compare_processed_text(individual_mode_full_text, entire_mode_full_text, delimiter=" "):
     # Calculate similarity ratio
-    from difflib import SequenceMatcher
-    similarity_ratio = SequenceMatcher(None, text_individual, text_entire).ratio()
+    similarity_ratio = SequenceMatcher(None, individual_mode_full_text, entire_mode_full_text).ratio()
 
     print(f"similarity_ratio: {similarity_ratio}")
 
-    import nltk
-
-    # Download the required resources (run this once)
-    nltk.download('punkt')
-
     # Tokenize the text into words
-    word_list_individual = nltk.word_tokenize(text_individual)
+    word_list_individual = nltk.word_tokenize(individual_mode_full_text)
     n_word_list_individual = len(word_list_individual)
     print("n_word_list_in_text_individual:", n_word_list_individual)
     word_sets_individual = set(word_list_individual)
@@ -122,7 +191,7 @@ def run(filename, page_limit=None, drawable=True):
     print(f"n_word_sets_in_text_individual: {n_word_sets_individual}")
     # print("word_sets_merged:", word_sets_merged)
 
-    word_list_entire = nltk.word_tokenize(text_entire)
+    word_list_entire = nltk.word_tokenize(entire_mode_full_text)
     n_word_list_entire = len(word_list_entire)
     print("n_word_list_individual:", n_word_list_entire)
     word_sets_entire = set(word_list_entire)
@@ -137,49 +206,40 @@ def run(filename, page_limit=None, drawable=True):
     print(f"unique_words_in_text_individual: {unique_words_individual}\n")
     print(f"unique_words_in_text_entire: {unique_words_entire}")
 
-    report = {
-        "file_info": {
-            "filename": filename,
-            "n_pages": n_pages,
-            "page_size": page_size,
+    return {
+        "similarity_ratio": similarity_ratio,
+        "individual_blocks": {
+            "n_word_list": n_word_list_individual,
+            "n_word_sets": n_word_sets_individual,
+            "unique_words": delimiter.join(list(unique_words_individual)),
         },
-        "model": {
-            "model": str(model),
-            "model_type": model_type,
-        },
-        "processing_time": {
-            "individual_blocks": infer_time_individual,
-            "entire_page": infer_time_entire,
-        },
-        "text_similarity": {
-            "similarity_ratio": similarity_ratio,
-            "individual_blocks": {
-                "n_word_list": n_word_list_individual,
-                "n_word_sets": n_word_sets_individual,
-                "unique_words": delimiter.join(list(unique_words_individual)),
-            },
-            "entire_page": {
-                "n_word_list": n_word_list_entire,
-                "n_word_sets": n_word_sets_entire,
-                "unique_words": delimiter.join(list(unique_words_entire)),
-            },
-        },
-        "extracted_text": {
-            "individual_blocks": {
-                "text_by_page": text_individual_dict,
-                "text_all": text_individual,
-            },
-            "entire_page": {
-                "text_by_page": text_entire_dict,
-                "text_all": text_entire,
-            },
-        },
+        "entire_page": {
+            "n_word_list": n_word_list_entire,
+            "n_word_sets": n_word_sets_entire,
+            "unique_words": delimiter.join(list(unique_words_entire)),
+        }
     }
 
+
+def write_report(report, now_str):
     report_f_name = f"validate-ocr-{now_str}.json"
     report_f_path = os.path.join(output_dir, report_f_name)
     with open(report_f_path, "w", encoding="utf-8-sig") as f:
         json.dump(report, f, indent=4)
+
+
+def run():
+    filenames = [
+        # "2023-Jan-economic-outlook.pdf",
+        # "recalibrating-risk-report.pdf",
+        # "Silent-Giant.pdf",
+        # "loremipsum_multipage.pdf",
+        "layout-parser-paper-fast.pdf",
+    ]
+
+    for f_name in filenames:
+        # run_analysis(f_name, None, True)
+        validate_performance(f_name)
 
 
 if __name__ == '__main__':
@@ -191,13 +251,4 @@ if __name__ == '__main__':
     output_dir = os.path.join(cur_dir, "output")
     os.makedirs(output_dir, exist_ok=True)
 
-    filenames = [
-        "2023-Jan-economic-outlook.pdf",
-        "recalibrating-risk-report.pdf",
-        "Silent-Giant.pdf",
-        "loremipsum_multipage.pdf",
-        "layout-parser-paper.pdf",
-    ]
-
-    for f_name in filenames:
-        run(f_name, None, True)
+    run()
