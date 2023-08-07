97ed453ef8ce34783cc681b6389b425fd1a65401
christinestraub <christinemstraub@gmail.com>
2023-08-07 12:47:47 -0700

    feat: add functionality to validate ocr performance by `image` mode to `validate_ocr_performance` module

--- examples/ocr/validate_ocr_performance.py
+++ examples/ocr/validate_ocr_performance.py
@@ -1,117 +1,22 @@
 import json
 import os
-import tempfile
 import time
 from datetime import datetime
 from difflib import SequenceMatcher
 
-import pdf2image
 import nltk
-from engine import run_ocr_with_layout_detection
-from unstructured_inference.inference.layout import process_file_with_model
+import pdf2image
 
-from unstructured_inference.models.base import get_model
-from unstructured_inference.models.unstructuredmodel import (
-    UnstructuredElementExtractionModel,
-    UnstructuredObjectDetectionModel,
-)
+from unstructured_inference.inference.layout import process_file_with_model, create_image_output_dir, DocumentLayout
 
 # Download the required resources (run this once)
 nltk.download('punkt')
 
 
-def run_analysis(filename, page_limit=None, drawable=True):
-    print(">>> Start", filename)
-
-    now_dt = datetime.utcnow()
-    now_str = now_dt.strftime("%Y_%m_%d-%H_%M_%S")
-
-    f_path = os.path.join(example_docs_dir, filename)
-
-    sub_output_dir = os.path.join(output_dir, now_str)
-    if drawable:
-        os.makedirs(sub_output_dir, exist_ok=True)
-
-    with tempfile.TemporaryDirectory() as tmpdir:
-        images = pdf2image.convert_from_path(f_path, output_folder=tmpdir)
-
-    individual_page_img_paths = []
-    individual_page_images = []
-    for i, image in enumerate(images):
-        # Save the image to a file
-        # img_path = os.path.join(sub_output_dir, f"page_{i+1}.jpg")
-        # image.save(img_path)
-        # individual_page_img_paths.append(img_path)
-
-        individual_page_images.append(image)
-
-    n_pages = len(individual_page_images)
-    print(f"number_of_pages: {n_pages}")
-
-    page_limit = page_limit if page_limit else n_pages
-
-    print("individual_page_images:")
-    for i, image in enumerate(individual_page_images[:page_limit]):
-        print(f"\timage{i + 1} - size: {image.size}")
-
-    page_size = individual_page_images[0].size if len(individual_page_images) > 0 else None
-
-    model = get_model()
-    if isinstance(model, UnstructuredObjectDetectionModel):
-        detection_model = model
-        element_extraction_model = None
-        model_type = "UnstructuredObjectDetectionModel"
-    elif isinstance(model, UnstructuredElementExtractionModel):
-        detection_model = None
-        element_extraction_model = model
-        model_type = "UnstructuredElementExtractionModel"
-    else:
-        raise ValueError(f"Unsupported model type: {type(model)}")
-
-    print("model_type:", model_type)
-
-    # OCR'ing individual blocks
-    print("OCR'ing individual blocks...")
-
-    individual_page_images = individual_page_images[:page_limit]
-
-    infer_time_individual, text_individual_dict = run_ocr_with_layout_detection(
-        images=individual_page_images,
-        detection_model=detection_model,
-        element_extraction_model=element_extraction_model,
-        mode="individual_blocks",
-        output_dir=sub_output_dir,
-        drawable=drawable,
-    )
-
-    # OCR'ing entire page
-    print("OCR'ing entire page...")
-
-    infer_time_entire, text_entire_dict = run_ocr_with_layout_detection(
-        images=individual_page_images,
-        detection_model=detection_model,
-        element_extraction_model=element_extraction_model,
-        mode="entire_page",
-        output_dir=sub_output_dir,
-        drawable=drawable,
-    )
-
-    print("Processing Time (OCR'ing individual blocks)")
-    print(f"\ttotal_infer_time: {infer_time_individual}")
-    print(f"\tavg_infer_time_per_page: {infer_time_individual / n_pages}")
-
-    print("Processing Time (OCR'ing entire page)")
-    print(f"\ttotal_infer_time: {infer_time_entire}")
-    print(f"\tavg_infer_time_per_page: {infer_time_entire / n_pages}")
-
-    delimiter = " "
-
-    text_individual = delimiter.join(text_individual_dict.values())
-    text_entire = delimiter.join(text_entire_dict.values())
-
-
 def validate_performance(
     f_name,
+    validation_mode,
+    is_image_file=False,
 ):
     print(">>> Start performance comparison - filename:", f_name)
 
@@ -119,17 +24,53 @@ def validate_performance(
     now_str = now_dt.strftime("%Y_%m_%d-%H_%M_%S")
 
     f_path = os.path.join(example_docs_dir, f_name)
-    pdf_info = pdf2image.pdfinfo_from_path(f_path)
+
+    image_f_paths = []
+    if validation_mode == "pdf":
+        pdf_info = pdf2image.pdfinfo_from_path(f_path)
+        n_pages = pdf_info["Pages"]
+    elif validation_mode == "image":
+        if is_image_file:
+            image_f_paths.append(f_path)
+        else:
+            image_output_dir = create_image_output_dir(f_path)
+            images = pdf2image.convert_from_path(f_path, output_folder=image_output_dir)
+            image_f_paths = [image.filename for image in images]
+        n_pages = len(image_f_paths)
+    else:
+        n_pages = 0
 
     processing_result = {}
     for ocr_mode in ["individual_blocks", "entire_page"]:
         start_time = time.time()
-        layout = process_file_with_model(
-            f_path,
-            model_name=None,
-            ocr_mode=ocr_mode,
-        )
+
+        if validation_mode == "pdf":
+            layout = process_file_with_model(
+                f_path,
+                model_name=None,
+                ocr_mode=ocr_mode,
+            )
+        elif validation_mode == "image":
+            pages = []
+            for image_f_path in image_f_paths:
+                _layout = process_file_with_model(
+                    image_f_path,
+                    model_name=None,
+                    ocr_mode=ocr_mode,
+                    is_image=True,
+                )
+                pages += _layout.pages
+            for i, page in enumerate(pages):
+                page.number = i + 1
+            layout = DocumentLayout.from_pages(pages)
+        else:
+            layout = None
+
         infer_time = time.time() - start_time
+
+        if layout is None:
+            print("Layout is None")
+            return
 
         full_text = str(layout)
         page_text = {}
@@ -152,7 +93,7 @@ def validate_performance(
     report = {
         "file_info": {
             "filename": f_name,
-            "n_pages": pdf_info["Pages"],
+            "n_pages": n_pages,
         },
         "processing_time": {
             "individual_blocks": processing_result["individual_blocks"]["infer_time"],
@@ -171,7 +112,7 @@ def validate_performance(
         },
     }
 
-    write_report(report, now_str)
+    write_report(report, now_str, validation_mode)
 
     print("<<< End performance comparison", f_name)
 
@@ -221,25 +162,29 @@ def compare_processed_text(individual_mode_full_text, entire_mode_full_text, del
     }
 
 
-def write_report(report, now_str):
-    report_f_name = f"validate-ocr-{now_str}.json"
+def write_report(report, now_str, validation_mode):
+    report_f_name = f"validate-ocr-{validation_mode}-{now_str}.json"
     report_f_path = os.path.join(output_dir, report_f_name)
     with open(report_f_path, "w", encoding="utf-8-sig") as f:
         json.dump(report, f, indent=4)
 
 
 def run():
-    filenames = [
-        # "2023-Jan-economic-outlook.pdf",
-        # "recalibrating-risk-report.pdf",
-        # "Silent-Giant.pdf",
-        # "loremipsum_multipage.pdf",
-        "layout-parser-paper-fast.pdf",
+    test_files = [
+        # {"name": "layout-parser-paper-fast.pdf", "mode": "image", "is_image_file": False},
+        {"name": "layout-parser-paper-fast.jpg", "mode": "image", "is_image_file": True},
+        # {"name": "loremipsum_multipage.pdf", "mode": "pdf", "is_image_file": False},
+        # {"name": "2023-Jan-economic-outlook.pdf", "mode": "pdf", "is_image_file": False},
+        # {"name": "recalibrating-risk-report.pdf", "mode": "pdf", "is_image_file": False},
+        # {"name": "Silent-Giant.pdf", "mode": "pdf", "is_image_file": False},
     ]
 
-    for f_name in filenames:
-        # run_analysis(f_name, None, True)
-        validate_performance(f_name)
+    for test_file in test_files:
+        f_name = test_file["name"]
+        validation_mode = test_file["mode"]
+        is_image_file = test_file["is_image_file"]
+
+        validate_performance(f_name, validation_mode, is_image_file)
 
 
 if __name__ == '__main__':
