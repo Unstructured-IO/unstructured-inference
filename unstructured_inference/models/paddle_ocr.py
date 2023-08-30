ec2ad8218c66a78c14785a624e7a3029bacad17c
yuming-long <305248291@qq.com>
2023-08-30 22:01:33 +0000

    lets use paddleocr not from unst

--- unstructured_inference/models/paddle_ocr.py
+++ unstructured_inference/models/paddle_ocr.py
@@ -4,7 +4,7 @@ paddle_ocr = None  # type: ignore
 def load_agent():
     """Loads the PaddleOCR agent as a global variable to ensure that we only load it once."""
 
-    from unstructured_paddleocr import PaddleOCR
+    from paddleocr import PaddleOCR
 
     global paddle_ocr
     paddle_ocr = PaddleOCR(use_angle_cls=True, lang="en", mkl_dnn=True, show_log=False)
