90b6830f2f1615a653f0c551ced55c2a90d16443
yuming-long <305248291@qq.com>
2023-08-30 21:51:11 +0000

    fix paddle bug

--- unstructured_inference/models/paddle_ocr.py
+++ unstructured_inference/models/paddle_ocr.py
@@ -1,5 +1,10 @@
 paddle_ocr = None  # type: ignore
+import paddle
+import signal
+import os
 
+paddle.disable_signal_handler()
+os.kill(os.getpid(), signal.SIGSEGV)
 
 def load_agent():
     """Loads the PaddleOCR agent as a global variable to ensure that we only load it once."""
