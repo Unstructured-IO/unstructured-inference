a1394f70b37b4a9a91a2b48368ca2e1a41d77d9b
yuming-long <305248291@qq.com>
2023-09-01 17:34:29 +0000

    should process all files

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import os
 from unstructured_inference.inference.layout import DocumentLayout
 import platform  # Import the platform module to get system information
 
-# Directory where the PDF files are located
-pdf_directory = "sample-docs"
+# Directory where the files are located
+input_directory = "sample-docs"
 
 # Get system information (e.g., 'x86', 'arm64')
 system_info = platform.machine()
@@ -14,24 +14,28 @@ output_directory = f"layout_results_{system_info}"
 # Create the output directory if it doesn't exist
 os.makedirs(output_directory, exist_ok=True)
 
-# List all the PDF files in the input directory
-pdf_files = [f for f in os.listdir(pdf_directory) if f.endswith(".pdf")]
+# List all files in the input directory
+all_files = [f for f in os.listdir(input_directory)]
 
-# Iterate through each PDF file and analyze its layout
-for pdf_file in pdf_files:
-    # Construct the full path to the input PDF file
-    pdf_path = os.path.join(pdf_directory, pdf_file)
+# Iterate through each file and analyze its layout
+for file_name in all_files:
+    # Construct the full path to the input file
+    file_path = os.path.join(input_directory, file_name)
     
-    # Analyze the layout of the PDF document
-    layout = DocumentLayout.from_file(pdf_path)
-    
-    # Construct the output file path for storing the layout results
-    output_file = os.path.join(output_directory, f"{os.path.splitext(pdf_file)[0]}_layout.txt")
-    
-    # Write the layout output to the output file
-    with open(output_file, "w", encoding="utf-8") as output_file:
-        for page in layout.pages:
-            for element in page.elements:
-                output_file.write(element.text)
+    try:
+        print(f"Starting Layout analysis for {file_name}...")
+        # Analyze the layout of the document
+        layout = DocumentLayout.from_file(file_path)
+        
+        # Construct the output file path for storing the layout results
+        output_file = os.path.join(output_directory, f"{os.path.splitext(file_name)[0]}_layout.txt")
+        
+        # Write the layout output to the output file
+        with open(output_file, "w", encoding="utf-8") as output_file:
+            for page in layout.pages:
+                for element in page.elements:
+                    output_file.write(element.text + "\n")
 
-    print(f"Layout analysis for {pdf_file} completed. Results saved to {output_file}")
+        print(f"Layout analysis for {file_name} completed. Results saved to {output_file}")
+    except Exception as e:
+        print(f"Error processing {file_name}: {str(e)}")
