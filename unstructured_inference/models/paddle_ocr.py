763afbb0266cb32363d4063fa1604c6d056690b0
yuming <305248291@qq.com>
2023-09-08 21:35:44 -0400

    set gpu device

--- unstructured_inference/models/paddle_ocr.py
+++ unstructured_inference/models/paddle_ocr.py
@@ -6,7 +6,8 @@ def load_agent():
 
     from unstructured_paddleocr import PaddleOCR
     import paddle
-    gpu_available  = paddle.device.is_compiled_with_cuda()
+    # Use paddlepaddle-gpu if there is gpu device available
+    gpu_available  = (paddle.device.cuda.device_count() > 0)
 
     global paddle_ocr
     paddle_ocr = PaddleOCR(use_angle_cls=True, use_gpu=gpu_available, lang="en", mkl_dnn=True, show_log=False)
