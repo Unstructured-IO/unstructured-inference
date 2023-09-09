0e860a8686c4983e2dcfec0ebcec369619db3efb
yuming <305248291@qq.com>
2023-09-09 18:56:59 -0400

    should allow to pass in lang for ocr instance

--- unstructured_inference/models/paddle_ocr.py
+++ unstructured_inference/models/paddle_ocr.py
@@ -1,11 +1,12 @@
+from unstructured_paddleocr import PaddleOCR
+import paddle
+
 paddle_ocr = None  # type: ignore
 
 
-def load_agent():
+def load_agent(language: str = "en"):
     """Loads the PaddleOCR agent as a global variable to ensure that we only load it once."""
 
-    from unstructured_paddleocr import PaddleOCR
-    import paddle
     # Disable signal handlers at C++ level upon failing
     # ref: https://www.paddlepaddle.org.cn/documentation/docs/en/api/paddle/disable_signal_handler_en.html#disable-signal-handler
     paddle.disable_signal_handler()
@@ -13,6 +14,6 @@ def load_agent():
     gpu_available  = (paddle.device.cuda.device_count() > 0)
 
     global paddle_ocr
-    paddle_ocr = PaddleOCR(use_angle_cls=True, use_gpu=gpu_available, lang="en", enable_mkldnn=True, show_log=False)
+    paddle_ocr = PaddleOCR(use_angle_cls=True, use_gpu=gpu_available, lang=language, enable_mkldnn=True, show_log=False)
 
     return paddle_ocr
