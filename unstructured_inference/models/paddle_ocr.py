dfa0bd3f6d990548f88572c11bc2e94bb522cef4
yuming <305248291@qq.com>
2023-09-08 20:25:34 -0400

    specficy use_gpu var and correct lang param name

--- unstructured_inference/models/paddle_ocr.py
+++ unstructured_inference/models/paddle_ocr.py
@@ -5,8 +5,10 @@ def load_agent():
     """Loads the PaddleOCR agent as a global variable to ensure that we only load it once."""
 
     from unstructured_paddleocr import PaddleOCR
+    import paddle
+    gpu_available  = paddle.device.is_compiled_with_cuda()
 
     global paddle_ocr
-    paddle_ocr = PaddleOCR(use_angle_cls=True, lang="en", mkl_dnn=True, show_log=False)
+    paddle_ocr = PaddleOCR(use_angle_cls=True, use_gpu=gpu_available, lang="en", mkl_dnn=True, show_log=False)
 
     return paddle_ocr
