14ad7996502bd26ee72e00b09eb836a0d3dcfce1
yuming <305248291@qq.com>
2023-09-08 22:02:30 -0400

    sync

--- unstructured_inference/models/paddle_ocr.py
+++ unstructured_inference/models/paddle_ocr.py
@@ -6,10 +6,13 @@ def load_agent():
 
     from unstructured_paddleocr import PaddleOCR
     import paddle
+    # Disable signal handlers at C++ level upon failing
+    # ref: https://www.paddlepaddle.org.cn/documentation/docs/en/api/paddle/disable_signal_handler_en.html#disable-signal-handler
+    paddle.disable_signal_handler()
+    # Use paddlepaddle-gpu if there is gpu device available
     gpu_available  = (paddle.device.cuda.device_count() > 0)
 
     global paddle_ocr
-    paddle_ocr = PaddleOCR(use_angle_cls=True, use_gpu=gpu_available, lang="en", mkl_dnn=True, show_log=False)
+    paddle_ocr = PaddleOCR(use_angle_cls=True, use_gpu=gpu_available, lang="en", enable_mkldnn=True, show_log=False)
 
     return paddle_ocr
-
