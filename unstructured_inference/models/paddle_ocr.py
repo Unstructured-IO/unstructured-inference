a9e1e6641726c35dc15dfbe86c60cb3f8ce61a04
yuming-long <305248291@qq.com>
2023-09-09 01:36:51 +0000

    set gpu device

--- unstructured_inference/models/paddle_ocr.py
+++ unstructured_inference/models/paddle_ocr.py
@@ -5,8 +5,11 @@ def load_agent():
     """Loads the PaddleOCR agent as a global variable to ensure that we only load it once."""
 
     from unstructured_paddleocr import PaddleOCR
+    import paddle
+    gpu_available  = (paddle.device.cuda.device_count() > 0)
 
     global paddle_ocr
-    paddle_ocr = PaddleOCR(use_angle_cls=True, lang="en", mkl_dnn=True, show_log=False)
+    paddle_ocr = PaddleOCR(use_angle_cls=True, use_gpu=gpu_available, lang="en", mkl_dnn=True, show_log=False)
 
     return paddle_ocr
+
