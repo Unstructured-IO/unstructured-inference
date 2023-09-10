0a44d411fa995b47f34f6c3efa0dc0e86285c1dd
yuming <305248291@qq.com>
2023-09-10 19:57:34 -0400

    only called once?

--- unstructured_inference/models/paddle_ocr.py
+++ unstructured_inference/models/paddle_ocr.py
@@ -6,14 +6,15 @@ paddle_ocr = None  # type: ignore
 
 def load_agent(language: str = "en"):
     """Loads the PaddleOCR agent as a global variable to ensure that we only load it once."""
-
-    # Disable signal handlers at C++ level upon failing
-    # ref: https://www.paddlepaddle.org.cn/documentation/docs/en/api/paddle/disable_signal_handler_en.html#disable-signal-handler
-    paddle.disable_signal_handler()
-    # Use paddlepaddle-gpu if there is gpu device available
-    gpu_available  = (paddle.device.cuda.device_count() > 0)
-
     global paddle_ocr
-    paddle_ocr = PaddleOCR(use_angle_cls=True, use_gpu=gpu_available, lang=language, enable_mkldnn=True, show_log=False)
+    # Check if paddle_ocr has already been loaded
+    if paddle_ocr is None:
+        # Disable signal handlers at C++ level upon failing
+        # ref: https://www.paddlepaddle.org.cn/documentation/docs/en/api/paddle/disable_signal_handler_en.html#disable-signal-handler
+        paddle.disable_signal_handler()
+        # Use paddlepaddle-gpu if there is gpu device available
+        gpu_available  = (paddle.device.cuda.device_count() > 0)
+        
+        paddle_ocr = PaddleOCR(use_angle_cls=True, use_gpu=gpu_available, lang=language, enable_mkldnn=True, show_log=False)
 
     return paddle_ocr
