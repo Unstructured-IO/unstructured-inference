1fed263bb61cdcb98d2f7f50e06c491d68f23705
yuming-long <305248291@qq.com>
2023-09-12 16:45:38 +0000

    Revert "using lrucache for global var"
    This reverts commit fd994bf88fcf1153d114526f8145523b4bb1f4f5.

--- unstructured_inference/models/paddle_ocr.py
+++ unstructured_inference/models/paddle_ocr.py
@@ -1,12 +1,11 @@
-import functools
-
 import paddle
 from unstructured_paddleocr import PaddleOCR
 
+paddle_ocr = None  # type: ignore
 
-@functools.lru_cache(maxsize=None)
+
 def load_agent(language: str = "en"):
-    """Loads the PaddleOCR agent with caching to ensure that we only load it once."""
+    """Loads the PaddleOCR agent as a global variable to ensure that we only load it once."""
 
     # Disable signal handlers at C++ level upon failing
     # ref: https://www.paddlepaddle.org.cn/documentation/docs/en/api/paddle/
@@ -15,23 +14,25 @@ def load_agent(language: str = "en"):
     # Use paddlepaddle-gpu if there is gpu device available
     gpu_available = paddle.device.cuda.device_count() > 0
 
-    try:
-        # Enable MKL-DNN for paddle to speed up OCR if OS supports it
-        # ref: https://paddle-inference.readthedocs.io/en/master/
-        #      api_reference/cxx_api_doc/Config/CPUConfig.html
-        paddle_ocr = PaddleOCR(
-            use_angle_cls=True,
-            use_gpu=gpu_available,
-            lang=language,
-            enable_mkldnn=True,
-            show_log=False,
-        )
-    except AttributeError:
-        paddle_ocr = PaddleOCR(
-            use_angle_cls=True,
-            use_gpu=gpu_available,
-            lang=language,
-            enable_mkldnn=False,
-            show_log=False,
-        )
+    global paddle_ocr
+    if paddle_ocr is None:
+        try:
+            # Enable MKL-DNN for paddle to speed up OCR if OS supports it
+            # ref: https://paddle-inference.readthedocs.io/en/master/
+            #      api_reference/cxx_api_doc/Config/CPUConfig.html
+            paddle_ocr = PaddleOCR(
+                use_angle_cls=True,
+                use_gpu=gpu_available,
+                lang=language,
+                enable_mkldnn=True,
+                show_log=False,
+            )
+        except AttributeError:
+            paddle_ocr = PaddleOCR(
+                use_angle_cls=True,
+                use_gpu=gpu_available,
+                lang=language,
+                enable_mkldnn=False,
+                show_log=False,
+            )
     return paddle_ocr
