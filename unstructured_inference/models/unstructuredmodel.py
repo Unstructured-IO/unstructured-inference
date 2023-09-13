98436e370143d34d26e4e3a1782ea314c817724b
Benjamin Torres <benjamin@unstructured.io>
2023-09-12 18:57:17 -0600

    refactor: distribute changes for reducing overlapping across files

--- unstructured_inference/models/unstructuredmodel.py
+++ unstructured_inference/models/unstructuredmodel.py
@@ -2,16 +2,19 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from collections.abc import Iterable
-from typing import TYPE_CHECKING, Any, List, Union
+from typing import TYPE_CHECKING, Any, List
 
 import numpy as np
 from PIL.Image import Image
 
 from unstructured_inference.inference.elements import (
-    Rectangle,
+    grow_region_to_match_region,
     intersect_free_quadrilaterals,
     intersections,
+    partition_groups_from_regions,
 )
+from unstructured_inference.inference.layoutelement import probably_contained
+from unstructured_inference.utils import tag
 
 if TYPE_CHECKING:
     from unstructured_inference.inference.layoutelement import (
@@ -19,27 +22,24 @@ if TYPE_CHECKING:
         LocationlessLayoutElement,
     )
 
+import typing
 
-def draw(e1: LayoutElement, e2: LayoutElement):
+
+@typing.no_type_check
+def draw(e1: LayoutElement, e2: LayoutElement):  #
     import PIL.Image
     from PIL import ImageDraw, ImageFont
 
-    from unstructured_inference.inference.elements import minimal_containing_region
+    kbd = ImageFont.truetype("Keyboard.ttf", 20)
+    blank_page = PIL.Image.new("RGB", (2000, 2000))
+    draw = ImageDraw.Draw(blank_page)
 
-    try:
-        kbd = ImageFont.truetype("Keyboard.ttf", 20)
-        mini_page = minimal_containing_region(e1, e2)
-        blank_page = PIL.Image.new("RGB", (2000, 2000))
-        draw = ImageDraw.Draw(blank_page)
+    draw.text((e1.x1, e1.y1), text=f"{e1.text[:7]}", fill="white", font=kbd)
+    draw.rectangle((e1.x1, e1.y1, e1.x2, e1.y2), outline=(255, 0, 0))
 
-        draw.text((e1.x1, e1.y1), text=f"{e1.text[:7]}", fill="white", font=kbd)
-        draw.rectangle((e1.x1, e1.y1, e1.x2, e1.y2), outline=(255, 0, 0))
-
-        draw.text((e2.x1, e2.y1), text=f"{e2.text[:7]}", fill="white", font=kbd)
-        draw.rectangle((e2.x1, e2.y1, e2.x2, e2.y2), outline=(0, 255, 0))
-        blank_page.show()
-    except:
-        print("Ooops")
+    draw.text((e2.x1, e2.y1), text=f"{e2.text[:7]}", fill="white", font=kbd)
+    draw.rectangle((e2.x1, e2.y1, e2.x2, e2.y2), outline=(0, 255, 0))
+    blank_page.show()
 
 
 class UnstructuredModel(ABC):
@@ -89,33 +89,14 @@ class UnstructuredObjectDetectionModel(UnstructuredModel):
     def deduplicate_detected_elements(elements: List[LayoutElement]) -> List[LayoutElement]:
         """Deletes overlapping elements in a list of elements"""
 
-        from unstructured_inference.inference.elements import (
-            grow_region_to_match_region,
-            partition_groups_from_regions,
-        )
-
+        # DELETE, JUST FOR DEBUGGING
+        @typing.no_type_check
         def check_rectangle(e: LayoutElement):
             well_formed = e.x1 < e.x2 and e.y1 < e.y2
             if not well_formed:
                 print(f"Bad rectangle! {e.id}")
 
             return well_formed
-
-        def probably_contained(
-            element: Union[LayoutElement, Rectangle],
-            inside: Union[LayoutElement, Rectangle],
-            area_threshold: float = 0.2,
-        ) -> bool:
-            """This function checks if one element is inside other.
-             If is definetly inside returns True, in other case check
-            if the intersection is big enough."""
-            if Rectangle.a_inside_b(element, inside):
-                return True
-
-            intersected_area = element.intersection(inside)
-            if intersected_area:
-                return intersected_area.area >= element.area * area_threshold
-            return False
 
         def clean_tables(elements: List[LayoutElement]) -> Iterable[LayoutElement]:
             import numpy as np
@@ -142,20 +123,6 @@ class UnstructuredObjectDetectionModel(UnstructuredModel):
             final_elements.sort(key=lambda e: e.y1)
             return final_elements
 
-        def tag(elements):
-            colors = ["red", "blue", "green", "cyan", "magenta", "brown"]
-
-            max_x = 0
-            max_y = 0
-            for i, e in enumerate(elements):
-                e.text = f"-{i}-:{e.text}"
-                e.id = i
-                e.clrs = colors[i % 6]
-                max_x = max(max_x, e.x1)
-                max_x = max(max_x, e.x2)
-                max_y = max(max_y, e.y1)
-                max_x = max(max_y, e.y2)
-
         def shrink_regions(elements: List[LayoutElement], jump: int = 0) -> List[LayoutElement]:
             intersections_mtx = intersections(*elements)
 
@@ -167,7 +134,8 @@ class UnstructuredObjectDetectionModel(UnstructuredModel):
                 if not first:
                     continue
 
-                indices_to_check = np.where(row)[0]  # We get only the elements which intersected
+                # We get only the elements which intersected
+                indices_to_check = np.where(row)[0]
                 for j in indices_to_check:
                     if i != j and elements[j] is not None:
                         second = elements[j]
@@ -178,23 +146,24 @@ class UnstructuredObjectDetectionModel(UnstructuredModel):
                         second_inside_first = second.is_in(first)
 
                         if first_inside_second and not second_inside_first:
-                            elements[i] = None
+                            elements[i] = None  # type:ignore
                         elif second_inside_first and not first_inside_second:
                             # delete second element
-                            elements[j] = None
+                            elements[j] = None  # type:ignore
                         elif intersection:
                             iou = first.intersection_over_union(second)
                             if iou < 0.5:  # small
                                 first, second = intersect_free_quadrilaterals(first, second)
+                                # The rectangle is too small, delete
                                 if not check_rectangle(first) or first.height < 15:
-                                    elements[i] = None  # The rectangle is too small, delete
+                                    elements[i] = None  # type:ignore
                                 if not check_rectangle(second) or second.height < 15:
-                                    elements[j] = None
+                                    elements[j] = None  # type:ignore
 
                             else:  # big
                                 # merge
                                 grow_region_to_match_region(first, second)
-                                elements[j] = None
+                                elements[j] = None  # type:ignore
 
                 if elements[i] is None:  # the element have been deleted,
                     continue
@@ -204,10 +173,6 @@ class UnstructuredObjectDetectionModel(UnstructuredModel):
 
         tag(elements)
         cleaned_elements: List[LayoutElement] = []
-        print("Prechecking...")
-        for e in elements:
-            check_rectangle(e)
-        print("Precheck done.")
         # TODO: Delete nested elements with low or None probability
         # TODO: Keep most confident
         # TODO: Better to grow horizontally than vertically?
@@ -222,7 +187,7 @@ class UnstructuredObjectDetectionModel(UnstructuredModel):
             cleaned_elements.extend(g)  # type:ignore
 
         cleaned_elements = shrink_regions(cleaned_elements)
-        # cleaned_elements = [e for e in cleaned_elements if e.height>15]
+        cleaned_elements = [e for e in cleaned_elements if e.height > 15]
         return cleaned_elements
 
 
