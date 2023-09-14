5f66a5aa481110424cd5e0082b012958f883342b
yuming <305248291@qq.com>
2023-09-14 09:19:14 -0400

    replace global dance with cache

--- unstructured_inference/models/paddle_ocr.py
+++ unstructured_inference/models/paddle_ocr.py
@@ -1,9 +1,8 @@
 import paddle
+import functools
 from unstructured_paddleocr import PaddleOCR
 
-paddle_ocr = None  # type: ignore
-
-
+@functools.lru_cache(maxsize=None)
 def load_agent(language: str = "en"):
     """Loads the PaddleOCR agent as a global variable to ensure that we only load it once."""
 
@@ -14,25 +13,23 @@ def load_agent(language: str = "en"):
     # Use paddlepaddle-gpu if there is gpu device available
     gpu_available = paddle.device.cuda.device_count() > 0
 
-    global paddle_ocr
-    if paddle_ocr is None:
-        try:
-            # Enable MKL-DNN for paddle to speed up OCR if OS supports it
-            # ref: https://paddle-inference.readthedocs.io/en/master/
-            #      api_reference/cxx_api_doc/Config/CPUConfig.html
-            paddle_ocr = PaddleOCR(
-                use_angle_cls=True,
-                use_gpu=gpu_available,
-                lang=language,
-                enable_mkldnn=True,
-                show_log=False,
-            )
-        except AttributeError:
-            paddle_ocr = PaddleOCR(
-                use_angle_cls=True,
-                use_gpu=gpu_available,
-                lang=language,
-                enable_mkldnn=False,
-                show_log=False,
-            )
+    try:
+        # Enable MKL-DNN for paddle to speed up OCR if OS supports it
+        # ref: https://paddle-inference.readthedocs.io/en/master/
+        #      api_reference/cxx_api_doc/Config/CPUConfig.html
+        paddle_ocr = PaddleOCR(
+            use_angle_cls=True,
+            use_gpu=gpu_available,
+            lang=language,
+            enable_mkldnn=True,
+            show_log=False,
+        )
+    except AttributeError:
+        paddle_ocr = PaddleOCR(
+            use_angle_cls=True,
+            use_gpu=gpu_available,
+            lang=language,
+            enable_mkldnn=False,
+            show_log=False,
+        )
     return paddle_ocr
