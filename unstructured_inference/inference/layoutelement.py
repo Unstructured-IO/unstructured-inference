5d3b4cb588a020252a1d46e032a70e759020ee7e
Benjamin Torres <benjamin@unstructured.io>
2023-09-15 14:57:46 -0600

    fix: bad merge

--- unstructured_inference/inference/layoutelement.py
+++ unstructured_inference/inference/layoutelement.py
@@ -338,39 +338,42 @@ def get_elements_from_ocr_regions(ocr_regions: List[TextRegion]) -> List[LayoutE
 def separate(region_a: Union[LayoutElement, Rectangle], region_b: Union[LayoutElement, Rectangle]):
     """Reduce leftmost rectangle to don't overlap with the other"""
 
-    def reduce(keep:Rectangle,reduce:Rectangle):
-        #Asume intersection
-        
+    def reduce(keep: Rectangle, reduce: Rectangle):
+        # Asume intersection
+
         # Other is down
-        if reduce.y2>keep.y2 and reduce.x1<keep.x2:
+        if reduce.y2 > keep.y2 and reduce.x1 < keep.x2:
             # other is down-right
-            if reduce.x2>keep.x2 and reduce.y2>keep.y2:
-                reduce.y1=keep.y2 
+            if reduce.x2 > keep.x2 and reduce.y2 > keep.y2:
+                reduce.x1 = keep.x2 * 1.01
+                reduce.y1 = keep.y2 * 1.01
                 return
-            # other is down-left 
-            if reduce.x1<keep.x1 and reduce.y1<keep.y2:
-                reduce.x1=keep.y2
+            # other is down-left
+            if reduce.x1 < keep.x1 and reduce.y1 < keep.y2:
+                reduce.x1 = keep.y2
                 return
             # other is centered
-            reduce.y1=keep.y2
-        else: # other is up
+            reduce.y1 = keep.y2
+        else:  # other is up
             # other is up-right
-            if reduce.x2>keep.x2 and reduce.y1<keep.y1:
+            if reduce.x2 > keep.x2 and reduce.y1 < keep.y1:
                 reduce.y2 = keep.y1
                 return
-            # other is left 
-            if reduce.x1<keep.x1 and reduce.y1<keep.y1:
-                reduce.y2=keep.y1
+            # other is left
+            if reduce.x1 < keep.x1 and reduce.y1 < keep.y1:
+                reduce.y2 = keep.y1
                 return
             # other is centered
-            reduce.y2=keep.y1
+            reduce.y2 = keep.y1
+            reduce.y2 = keep.y1
+
     if not region_a.intersects(region_b):
         return
     else:
         if region_a.area>region_b.area:
             reduce(keep=region_a,reduce=region_b)
         else:
-            reduce(keep=region_b,reduce=region_a)
+            reduce(keep=region_b, reduce=region_a)
 
 
 # NOTE(alan): The right way to do this is probably to rewrite LayoutElement as well as the different
