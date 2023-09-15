d44c2f1a6e74960cb00bf6a3a884f34cee6b1903
Yao You <yao@unstructured.io>
2023-09-15 16:14:05 -0500

    more eval helper

--- unstructured_inference/models/eval.py
+++ unstructured_inference/models/eval.py
@@ -33,3 +33,34 @@ def eval_cells(actual_df: pd.DataFrame, predicted_df: pd.DataFrame) -> Dict[str,
                 scores[irow, icol] = fuzz.ratio(actual, pred_row[icol])
 
     return scores
+
+
+def join_df_content(df, joiner="\t"):
+    return joiner.join(df.values.ravel())
+
+
+def compare_contents_as_cells(actual_cells, pred_cells):
+    actual_df = table_cells_to_dataframe(actual_cells, 1, 1).fillna("")
+    pred_df = table_cells_to_dataframe(pred_cells, 1, 1).fillna("")
+    return compare_contents_as_df(actual_df, pred_df)
+
+
+def compare_contents_as_df(actual_df, pred_df, joiner="\t"):
+    """ravel the table as string then use text distance to compare the prediction against true
+    table;
+
+    When choosing joiner = "\t" (default) we evalute the structure as well as content (if a cell
+    with two words is split into two cells there is an extra "\t")
+
+    If choose joiner = " " (white space) we do not evaluate the structure but purely evalute if the
+    text is in the right order. A cell with two words could have been split into two cells each with
+    one word but the order still matches true table so it won't reduce the score
+    """
+    return {
+        "by_col": fuzz.partial_ratio_alignment(
+            join_df_content(actual_df), join_df_content(pred_df)
+        ),
+        "by_row": fuzz.partial_ratio_alignment(
+            join_df_content(actual_df.T), join_df_content(pred_df.T)
+        )
+    }
