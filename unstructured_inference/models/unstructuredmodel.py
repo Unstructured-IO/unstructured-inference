b6cf69636545882326f31096b3bb35fb71febc1e
Benjamin Torres <benjamin@unstructured.io>
2023-09-19 16:10:09 -0600

    fix(part1): simplified logic

--- unstructured_inference/models/unstructuredmodel.py
+++ unstructured_inference/models/unstructuredmodel.py
@@ -91,9 +91,9 @@ class UnstructuredObjectDetectionModel(UnstructuredModel):
                         key=lambda x: x[1],
                         reverse=True,
                     )  # sort elements by iom, so we first check the greatest
-                    indices_to_check = [x[0] for x in iom_to_check]  # type:ignore
+                    indices_to_check = [x[0] for x in iom_to_check if x[0] != i]  # type:ignore
             for j in indices_to_check:
-                if i != j and elements[j] is not None and elements[i] is not None:
+                if elements[j] is not None and elements[i] is not None:
                     second = elements[j]
                     intersection = first.intersection(
                         second,
@@ -130,7 +130,7 @@ class UnstructuredObjectDetectionModel(UnstructuredModel):
         of the type specified"""
         target_elements = [e for e in elements if e.type == type_to_clean]
         other_elements = [e for e in elements if e.type != type_to_clean]
-        if len(target_elements)==0 or len(other_elements) == 0:
+        if len(target_elements) == 0 or len(other_elements) == 0:
             return elements
 
         nested_check = None
