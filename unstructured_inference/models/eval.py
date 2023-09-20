1980a5a3fbae05d6bc7dd841fbfe7c028443d5f2
Yao You <yao@unstructured.io>
2023-09-20 09:37:52 -0500

    update interfece to accept both tab and row break tokens

--- unstructured_inference/models/eval.py
+++ unstructured_inference/models/eval.py
@@ -1,3 +1,4 @@
+from functools import partial
 from typing import Callable, Dict, List
 
 import numpy as np
@@ -12,9 +13,9 @@ EVAL_FUNCTIONS = {
 }
 
 
-def _join_df_content(df, joiner="\t"):
+def _join_df_content(df, tab_token="\t", row_break_token="\n"):
     """joining dataframe's table content as one long string"""
-    return joiner.join(df.values.ravel())
+    return row_break_token.join([tab_token.join(row) for row in df.values])
 
 
 def default_tokenizer(text: str) -> List[str]:
@@ -25,9 +26,10 @@ def default_tokenizer(text: str) -> List[str]:
 def compare_contents_as_df(
     actual_df: pd.DataFrame,
     pred_df: pd.DataFrame,
-    joiner: str = "\t",
     eval_func: str = "token_ratio",
     processor: Callable = None,
+    tab_token: str = "\t",
+    row_break_token: str = "\n",
 ):
     """ravel the table as string then use text distance to compare the prediction against true
     table
@@ -40,9 +42,6 @@ def compare_contents_as_df(
     pred_df: pd.DataFrame
         predicted table as pandas dataframe
 
-    joiner: str, default to "\t"
-        the string to join cells together
-
     eval_func: str, default tp "token_ratio"
         the eval_func should be one of "token_ratio", "ratio", "partial_token_ratio",
         "partial_ratio". Those are functions provided by rapidfuzz to evaluate text distances
@@ -54,6 +53,11 @@ def compare_contents_as_df(
         tokens eval functions we recommend using the `default_tokenizer` or some other functions to
         break down the text into words
 
+    tab_token: str, default to "\t"
+        the string to join cells together
+
+    row_break_token: str, default to "\n"
+        the string to join rows together
     """
     func = EVAL_FUNCTIONS.get(eval_func)
     if func is None:
@@ -61,15 +65,16 @@ def compare_contents_as_df(
             'eval_func must be one of "token_ratio", "ratio", "partial_token_ratio", '
             f'"partial_ratio" but got {eval_func}',
         )
+    join_func = partial(_join_df_content, tab_token=tab_token, row_break_token=row_break_token)
     return {
         f"by_col_{eval_func}": func(
-            _join_df_content(actual_df),
-            _join_df_content(pred_df),
+            join_func(actual_df),
+            join_func(pred_df),
             processor=processor,
         ),
         f"by_row_{eval_func}": func(
-            _join_df_content(actual_df.T),
-            _join_df_content(pred_df.T),
+            join_func(actual_df.T),
+            join_func(pred_df.T),
             processor=processor,
         ),
     }
