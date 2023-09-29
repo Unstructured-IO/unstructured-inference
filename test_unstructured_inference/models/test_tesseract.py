b64e546e5e8294ca2ca76ab98b3fca24b7641fbb
yuming <305248291@qq.com>
2023-09-29 12:13:28 -0400

    disable tesseract

--- test_unstructured_inference/models/test_tesseract.py
+++ test_unstructured_inference/models/test_tesseract.py
@@ -1,26 +1,26 @@
-from unittest.mock import patch
+# from unittest.mock import patch
 
-import pytest
+# import pytest
 
-from unstructured_inference.models import tesseract
-
-
-class MockTesseractAgent:
-    def __init__(self, languages):
-        pass
+# from unstructured_inference.models import tesseract
 
 
-def test_load_agent(monkeypatch):
-    monkeypatch.setattr(tesseract, "TesseractAgent", MockTesseractAgent)
-    monkeypatch.setattr(tesseract, "ocr_agents", {})
-
-    with patch.object(tesseract, "is_pytesseract_available", return_value=True):
-        tesseract.load_agent(languages="eng+swe")
-
-    assert isinstance(tesseract.ocr_agents["eng+swe"], MockTesseractAgent)
+# class MockTesseractAgent:
+#     def __init__(self, languages):
+#         pass
 
 
-def test_load_agent_raises_when_not_available():
-    with patch.object(tesseract, "is_pytesseract_available", return_value=False):
-        with pytest.raises(ImportError):
-            tesseract.load_agent()
+# def test_load_agent(monkeypatch):
+#     monkeypatch.setattr(tesseract, "TesseractAgent", MockTesseractAgent)
+#     monkeypatch.setattr(tesseract, "ocr_agents", {})
+
+#     with patch.object(tesseract, "is_pytesseract_available", return_value=True):
+#         tesseract.load_agent(languages="eng+swe")
+
+#     assert isinstance(tesseract.ocr_agents["eng+swe"], MockTesseractAgent)
+
+
+# def test_load_agent_raises_when_not_available():
+#     with patch.object(tesseract, "is_pytesseract_available", return_value=False):
+#         with pytest.raises(ImportError):
+#             tesseract.load_agent()
