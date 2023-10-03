28e1bc769e8597164931d32efb532e1643dddc21
yuming <305248291@qq.com>
2023-10-03 12:12:41 -0400

    remove ocr constant

--- unstructured_inference/constants.py
+++ unstructured_inference/constants.py
@@ -1,9 +1,9 @@
 from enum import Enum
 
 
-class OCRMode(Enum):
-    INDIVIDUAL_BLOCKS = "individual_blocks"
-    FULL_PAGE = "entire_page"
+# class OCRMode(Enum):
+#     INDIVIDUAL_BLOCKS = "individual_blocks"
+#     FULL_PAGE = "entire_page"
 
 
 class AnnotationResult(Enum):
@@ -15,11 +15,11 @@ class Source(Enum):
     YOLOX = "yolox"
     DETECTRON2_ONNX = "detectron2_onnx"
     DETECTRON2_LP = "detectron2_lp"
-    OCR_TESSERACT = "OCR-tesseract"
-    OCR_PADDLE = "OCR-paddle"
+    # OCR_TESSERACT = "OCR-tesseract"
+    # OCR_PADDLE = "OCR-paddle"
     PDFMINER = "pdfminer"
     MERGED = "merged"
 
 
-SUBREGION_THRESHOLD_FOR_OCR = 0.5
+# SUBREGION_THRESHOLD_FOR_OCR = 0.5
 FULL_PAGE_REGION_THRESHOLD = 0.99
