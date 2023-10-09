a96e574c77764f27e07d6c476274a78aef4b618b
christinestraub <christinemstraub@gmail.com>
2023-10-09 14:54:33 -0700

    feat: update `remove_control_characters` to replace newline character with a space

--- unstructured_inference/inference/elements.py
+++ unstructured_inference/inference/elements.py
@@ -214,8 +214,8 @@ class TextRegion(Rectangle):
             text = aggregate_by_block(self, objects)
         else:
             text = ""
-        text = remove_control_characters(text)
-        return text
+        cleaned_text = remove_control_characters(text)
+        return cleaned_text
 
 
 class EmbeddedTextRegion(TextRegion):
@@ -276,6 +276,10 @@ def is_cid_present(text: str) -> bool:
 
 def remove_control_characters(text: str) -> str:
     """Removes control characters from text."""
+
+    # Replace newline character with a space
+    text = text.replace("\n", " ")
+    # Remove other control characters
     out_text = "".join(c for c in text if unicodedata.category(c)[0] != "C")
     return out_text
 
