90072b77d4ff728e3a029c2b63f118f952c3316e
Alan Bertl <alan@unstructured.io>
2023-10-20 02:45:25 -0500

    Suppress kwargs when not used

--- unstructured_inference/inference/layout.py
+++ unstructured_inference/inference/layout.py
@@ -71,13 +71,8 @@ class DocumentLayout:
     def from_file(
         cls,
         filename: str,
-        detection_model: Optional[UnstructuredObjectDetectionModel] = None,
-        element_extraction_model: Optional[UnstructuredElementExtractionModel] = None,
         fixed_layouts: Optional[List[Optional[List[TextRegion]]]] = None,
-        extract_tables: bool = False,
         pdf_image_dpi: int = 200,
-        extract_images_in_pdf: bool = False,
-        image_output_dir_path: Optional[str] = None,
         **kwargs,
     ) -> DocumentLayout:
         """Creates a DocumentLayout from a pdf file."""
@@ -110,13 +105,8 @@ class DocumentLayout:
                         image,
                         number=i + 1,
                         document_filename=filename,
-                        detection_model=detection_model,
-                        element_extraction_model=element_extraction_model,
                         layout=layout,
                         fixed_layout=fixed_layout,
-                        extract_tables=extract_tables,
-                        extract_images_in_pdf=extract_images_in_pdf,
-                        image_output_dir_path=image_output_dir_path,
                         **kwargs,
                     )
                     pages.append(page)
@@ -457,12 +447,6 @@ class PageLayout:
 def process_data_with_model(
     data: BinaryIO,
     model_name: Optional[str],
-    is_image: bool = False,
-    fixed_layouts: Optional[List[Optional[List[TextRegion]]]] = None,
-    extract_tables: bool = False,
-    pdf_image_dpi: int = 200,
-    extract_images_in_pdf: bool = False,
-    image_output_dir_path: Optional[str] = None,
     **kwargs,
 ) -> DocumentLayout:
     """Processes pdf file in the form of a file handler (supporting a read method) into a
@@ -473,12 +457,6 @@ def process_data_with_model(
         layout = process_file_with_model(
             tmp_file.name,
             model_name,
-            is_image=is_image,
-            fixed_layouts=fixed_layouts,
-            extract_tables=extract_tables,
-            pdf_image_dpi=pdf_image_dpi,
-            extract_images_in_pdf=extract_images_in_pdf,
-            image_output_dir_path=image_output_dir_path,
             **kwargs,
         )
 
