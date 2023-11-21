0f0c2be07c673615217b214937f50a091676a8aa
pravin-unstructured <129794394+pravin-unstructured@users.noreply.github.com>
2023-11-21 13:29:00 -0500

    fix bug which allows label map from initialize params to be used from… (#293)
    … reading in a json file.

--- unstructured_inference/models/super_gradients.py
+++ unstructured_inference/models/super_gradients.py
@@ -69,15 +69,15 @@ class UnstructuredSuperGradients(UnstructuredObjectDetectionModel):
                 class_id = pred_classes[image_index, i]
                 prob = pred_scores[image_index, i]
                 x1, y1, x2, y2 = pred_boxes[image_index, i]
-                detected_class = self.layout_classes[int(class_id)]
+                detected_class = self.layout_classes[str(class_id)]
                 region = LayoutElement.from_coords(
-                    x1,
-                    y1,
-                    x2,
-                    y2,
+                    float(x1),
+                    float(y1),
+                    float(x2),
+                    float(y2),
                     text=None,
                     type=detected_class,
-                    prob=prob,
+                    prob=float(prob),
                     source=Source.SUPER_GRADIENTS,
                 )
                 regions.append(cast(LayoutElement, region))
