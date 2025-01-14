41c6a7d17c94dddb5ce8f9237c8551ce10550799
Yao You <yao@unstructured.io>
2025-01-14 17:24:25 -0600

    fix: fix bugs in data structure
    - fix bug when an empty list is passed into TextRegions.from_list
    - fix bug when concatenating a list of `LayoutElements` the class id
      maps is not updated correctly

--- test_unstructured_inference/test_elements.py
+++ test_unstructured_inference/test_elements.py
@@ -441,3 +441,31 @@ def test_layoutelements_to_list_and_back(test_layoutelements):
 def test_layoutelements_from_list_no_elements():
     back = LayoutElements.from_list(elements=[])
     assert back.source is None
+    assert back.element_coords.size == 0
+
+
+def test_textregions_from_list_no_elements():
+    back = TextRegions.from_list(regions=[])
+    assert back.source is None
+    assert back.element_coords.size == 0
+
+
+def test_layoutelements_concatenate():
+    layout1 = LayoutElements(
+        element_coords=np.array([[0, 0, 1, 1], [1, 1, 2, 2]]),
+        texts=np.array(["a", "two"]),
+        source=None,
+        element_class_ids=np.array([0, 1]),
+        element_class_id_map={0: "type0", 1: "type1"},
+    )
+    layout2 = LayoutElements(
+        element_coords=np.array([[10, 10, 2, 2], [20, 20, 1, 1]]),
+        texts=np.array(["three", "4"]),
+        source=None,
+        element_class_ids=np.array([0, 1]),
+        element_class_id_map={0: "type1", 1: "type2"},
+    )
+    joint = LayoutElements.concatenate([layout1, layout2])
+    assert joint.texts.tolist() == ["a", "two", "three", "4"]
+    assert joint.element_class_ids.tolist() == [0, 1, 1, 2]
+    assert joint.element_class_id_map == {0: "type0", 1: "type1", 2: "type2"}
