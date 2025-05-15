a1e045e59d269f34497ad0976551296bd708e020
Antonio Jimeno Yepes <antonio@unstructured.io>
2025-05-15 12:16:05 -0700

    Fix version

--- unstructured_inference/__version__.py
+++ unstructured_inference/__version__.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 __version__ = "1.0.0"  # pragma: no cover
-=======
-__version__ = "1.0.0"  # pragma: no cover
->>>>>>> 2af4d3a (Updated support for 3.10+)
-=======
-__version__ = "1.0.0"  # pragma: no cover
->>>>>>> e93ac0c4c322a2cd7b903e02dff27c93e816898b
