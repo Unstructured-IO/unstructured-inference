97d5c8561a8760593ac86129669c24933de15141
Alan Bertl <alan@unstructured.io>
2026-01-13 15:43:10 -0600

    fix typing

--- unstructured_inference/inference/layout.py
+++ unstructured_inference/inference/layout.py
@@ -432,7 +432,9 @@ def convert_pdf_to_image(
                 bitmap.close()
     finally:
         pdf.close()
-    if not path_only:
+    if not output_folder:
+        if path_only:
+            raise ValueError("output_folder must be specified if path_only is true")
         return list(images.values())
     else:
         # Save images to output_folder
@@ -443,4 +445,4 @@ def convert_pdf_to_image(
             fn: str = os.path.join(str(output_folder), f"page_{i}.png")
             image.save(fn)
             filenames.append(fn)
-        return filenames
+        return filenames if path_only else list[Image.Image](images.values())
